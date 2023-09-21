7e03ff4fb8d4ce78f16cdc452fc141a3231296e4
sunyasheng <1044705955@qq.com>
2023-09-21 21:28:59 +0400

    up

--- main.py
+++ main.py
@@ -540,8 +540,10 @@ if __name__ == "__main__":
             model=model,
             model_parameters=param_groups,
             dist_init_required=False,
-            lr=model.learning_rate
         )
+        for param_group in model.optimizer.param_groups:
+            param_group['lr'] = model.learning_rate
+
         for name, param in model.named_parameters():
             param.global_name = name
         model_without_ddp = model
