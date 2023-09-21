15b4156aa4df4f15a8ece49ee465141fcbae43cd
sunyasheng <1044705955@qq.com>
2023-09-22 03:27:55 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_align.py
+++ stable_diffusion/ldm/models/diffusion/fmri_align.py
@@ -326,7 +326,7 @@ class FMRIAlign(nn.Module):
         fmri = batch['fmri'].half().cuda()
         # gt_image_vae = batch['image_vae'].half().cuda()
         gt_image = batch['image'].half().cuda()
-        # import pdb; pdb.set_trace();
+        import pdb; pdb.set_trace();
         gt_image = F.interpolate(gt_image, (256,256))
         gt_image_vae = self.encode_first_stage(gt_image).mode()
         pred_image_vae = self.fmri2visual_model(fmri)
