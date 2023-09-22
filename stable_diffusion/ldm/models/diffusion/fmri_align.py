6d72971a22ffba243e0377348a721de9aba76e92
sunyasheng <1044705955@qq.com>
2023-09-22 21:16:18 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_align.py
+++ stable_diffusion/ldm/models/diffusion/fmri_align.py
@@ -350,11 +350,11 @@ class FMRIAlign(nn.Module):
 
     def forward(self, batch, batch_idx, num_steps, *args, **kwargs):
         # x, c = self.get_input(batch, self.first_stage_key)
-        # fmri = batch['fmri'].half().cuda()
-        fmri = batch['fmri'].cuda()
+        fmri = batch['fmri'].half().cuda()
+        # fmri = batch['fmri'].cuda()
         # gt_image_vae = batch['image_vae'].half().cuda()
-        # gt_image = batch['image'].half().cuda()
-        gt_image = batch['image'].cuda()
+        gt_image = batch['image'].half().cuda()
+        # gt_image = batch['image'].cuda()
         # import pdb; pdb.set_trace();
         gt_image = F.interpolate(gt_image, (256,256))
         gt_image_vae = self.encode_first_stage(gt_image).mode()
