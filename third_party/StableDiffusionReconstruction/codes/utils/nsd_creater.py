5b5b533e95463a863c55b1eaa8fd4d8118fd39de
sunyasheng <1044705955@qq.com>
2023-09-23 15:30:31 +0400

    up

--- third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
+++ third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
@@ -168,7 +168,7 @@ class NSDDataset(Dataset):
         return nsd_dict
 
     def __len__(self):
-        if self.split == 'train': return self.X.shape[0]
-        else: return self.X_te.shape[0]
-        # return 100
+        # if self.split == 'train': return self.X.shape[0]
+        # else: return self.X_te.shape[0]
+        return 128
         # return 1
