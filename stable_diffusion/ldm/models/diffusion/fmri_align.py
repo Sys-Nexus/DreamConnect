08526663e4b3e5dde6a0541ae0f903dc3035e81a
sunyasheng <1044705955@qq.com>
2023-09-23 15:50:20 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_align.py
+++ stable_diffusion/ldm/models/diffusion/fmri_align.py
@@ -361,7 +361,7 @@ class FMRIAlign(nn.Module):
         pred_image_vae = self.fmri2visual_model(fmri)
         # pred_image_vae = pred_image_vae.view(fmri.shape[0],-1)
 
-        loss = torch.nn.MSELoss()(pred_image_vae, gt_image_vae)
+        loss = torch.nn.MSELoss()(pred_image_vae, gt_image_vae) * 0.1
         loss_dict = {'L1': loss.item()}
 
         # import pdb; pdb.set_trace();
