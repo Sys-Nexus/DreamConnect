30ea9331ad15bb35b138df5192b333332c6719ad
sunyasheng <1044705955@qq.com>
2023-09-29 22:23:07 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
+++ stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
@@ -1239,12 +1239,12 @@ class LatentDiffusion(DDPM):
         cond["c_crossattn"] = self.get_learned_conditioning(xc["c_crossattn"])
         cond["c_crossattn_1"] = self.get_learned_conditioning_fmri(xc["c_crossattn_1"])
 
-        import pdb; pdb.set_trace();
+        # import pdb; pdb.set_trace();
         uncond = {}
-        null_prompt = self.get_learned_conditioning([""]*2)
+        null_prompt = self.get_learned_conditioning([""]*N)
         fmri_null_prompt = self.get_learned_conditioning_fmri(torch.zeros_like(xc["c_crossattn_1"]))
-        uncond["c_crossattn"] = [null_prompt]
-        uncond["c_crossattn_1"] = [fmri_null_prompt]
+        uncond["c_crossattn"] = null_prompt
+        uncond["c_crossattn_1"] = fmri_null_prompt
 
         extra_args = {
             "cond": cond,
