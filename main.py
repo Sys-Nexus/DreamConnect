f133902b96ecd62cbe40465f397f5cc72c605b67
sunyasheng <1044705955@qq.com>
2023-09-30 12:20:29 +0400

    up

--- main.py
+++ main.py
@@ -575,9 +575,10 @@ if __name__ == "__main__":
 
     if config.model.params.fp16 and config.model.params.deepspeed == '':
         scaler = amp.GradScaler()
-        param_groups = model.parameters()
+        # param_groups = model.parameters()
         # param_groups = model.parameters()
         param_groups = [param for name, param in model.named_parameters() if param.requires_grad is True]
+        import pdb; pdb.set_trace();
     else:
         # scaler = None
         scaler = amp.GradScaler()
