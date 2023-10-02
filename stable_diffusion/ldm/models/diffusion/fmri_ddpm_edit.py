65fab17500d4d62ec2e7726d0a40aadc1f525af0
sunyasheng <1044705955@qq.com>
2023-10-02 18:47:55 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
+++ stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
@@ -998,7 +998,7 @@ class LatentDiffusion(DDPM):
             x_recon = fold(o) / normalization
 
         else:
-            import pdb; pdb.set_trace();
+            # import pdb; pdb.set_trace();
             x_recon = self.model(x_noisy, t, **cond)
 
         if isinstance(x_recon, tuple) and not return_ids:
