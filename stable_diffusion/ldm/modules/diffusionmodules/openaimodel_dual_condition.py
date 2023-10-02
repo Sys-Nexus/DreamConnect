46d65adc5a7cff75e686e26ef0bcedc21cd7a11a
sunyasheng <1044705955@qq.com>
2023-10-02 23:36:46 +0400

    up

--- stable_diffusion/ldm/modules/diffusionmodules/openaimodel_dual_condition.py
+++ stable_diffusion/ldm/modules/diffusionmodules/openaimodel_dual_condition.py
@@ -578,8 +578,8 @@ class UNetModel(nn.Module):
 
         if use_time_cond:
             self.time_embed_condtion = nn.Sequential(
-                nn.Conv1d(77, 77//2, 1, bias=True),
-                nn.Conv1d(77//2, 1, 1, bias=True),
+                # nn.Conv1d(1, 1, 1, bias=True),
+                nn.Conv1d(1, 1, 1, bias=True),
                 nn.Linear(context_dim, time_embed_dim, bias=True)
             ) if global_pool == False else nn.Linear(context_dim, time_embed_dim, bias=True)
 
