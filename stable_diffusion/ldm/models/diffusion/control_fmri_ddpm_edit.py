a95307dd458070e18ead75ebd9c76772d0c228f2
sunyasheng <1044705955@qq.com>
2023-10-02 17:36:10 +0400

    up

--- stable_diffusion/ldm/models/diffusion/control_fmri_ddpm_edit.py
+++ stable_diffusion/ldm/models/diffusion/control_fmri_ddpm_edit.py
@@ -444,7 +444,9 @@ class ControlLDM(LatentDiffusion):
             control_prompt = torch.cat(cond["c_crossattn_1"] , 1)
             ## TODO: hard code to set the hint to zero
             import pdb; pdb.set_trace();
-            fmri_control = self.control_model(x=x_noisy, hint=torch.zeros(size=(x_noisy.shape[0],3,256,256)).to(x_noisy), timesteps=t, context=control_prompt)
+            fmri_control = self.control_model(x=x_noisy.to(control_prompt.dtype), 
+                    hint=torch.zeros(size=(x_noisy.shape[0],3,256,256)).to(x_noisy).to(control_prompt.dtype),
+                    timesteps=t, context=control_prompt)
             fmri_control = [c * scale for c, scale in zip(fmri_control, self.control_scales)]
             cond["control"] = fmri_control
             ## only add above 
