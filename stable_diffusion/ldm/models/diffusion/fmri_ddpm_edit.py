9832360808365ef90c985ff72ce2d0551f736f5f
sunyasheng <1044705955@qq.com>
2023-10-02 22:17:34 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
+++ stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
@@ -695,15 +695,18 @@ class LatentDiffusion(DDPM):
         cond = {}
         random = torch.rand(x.size(0), device=z.device)
         prompt_mask = rearrange(random < uncond, "n -> n 1 1")
-        fmri_prompt_mask = (1 - (random >= uncond).float() * (random < uncond*2).float()).bool()
+        fmri_prompt_mask = (random >= uncond).float() * (random < uncond*2).float()
         fmri_prompt_mask = rearrange(fmri_prompt_mask, "n -> n 1 1")
         input_mask = 1 - rearrange((random >= uncond*2).float() * (random < uncond*3).float(), "n -> n 1 1 1")
         
         null_prompt = self.get_learned_conditioning([""])
         fmri_null_prompt = self.get_learned_conditioning_fmri(torch.zeros_like(xc["c_crossattn_1"]))
         fmri_learned_prompt = self.get_learned_conditioning_fmri(xc["c_crossattn_1"])        
+
+        # cond["c_crossattn_1"] = [torch.where(fmri_prompt_mask, fmri_null_prompt, fmri_learned_prompt)]
+
+        cond["c_crossattn_1"] = fmri_prompt_mask.float()*fmri_null_prompt + (1-fmri_prompt_mask.float())*fmri_learned_prompt
         import pdb;pdb.set_trace()
-        cond["c_crossattn_1"] = [torch.where(fmri_prompt_mask, fmri_null_prompt, fmri_learned_prompt)]
         cond["c_crossattn"] = [torch.where(prompt_mask, null_prompt, self.get_learned_conditioning(xc["c_crossattn"]).detach())]
 
         if self.is_fmri_input is True:
