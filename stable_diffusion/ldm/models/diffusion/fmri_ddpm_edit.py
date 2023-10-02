8704b573a3d79ad1630100889274f935e2b20c3b
sunyasheng <1044705955@qq.com>
2023-10-02 21:49:49 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
+++ stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
@@ -573,7 +573,7 @@ class LatentDiffusion(DDPM):
                 if isinstance(c, DiagonalGaussianDistribution):
                     c = c.mode()
             else:
-                import pdb; pdb.set_trace();
+                # import pdb; pdb.set_trace();
                 c = self.cond_stage_model_fmri(c)
         else:
             assert hasattr(self.cond_stage_model_fmri, self.cond_stage_forward_fmri)
@@ -701,9 +701,11 @@ class LatentDiffusion(DDPM):
         
         null_prompt = self.get_learned_conditioning([""])
         fmri_null_prompt = self.get_learned_conditioning_fmri(torch.zeros_like(xc["c_crossattn_1"]))
+        fmri_learned_prompt = self.get_learned_conditioning_fmri(xc["c_crossattn_1"])        
+        import pdb;pdb.set_trace()
+        cond["c_crossattn_1"] = [torch.where(fmri_prompt_mask, fmri_null_prompt, fmri_learned_prompt)]
         cond["c_crossattn"] = [torch.where(prompt_mask, null_prompt, self.get_learned_conditioning(xc["c_crossattn"]).detach())]
-        # import pdb;pdb.set_trace()
-        cond["c_crossattn_1"] = [torch.where(fmri_prompt_mask, fmri_null_prompt, self.get_learned_conditioning_fmri(xc["c_crossattn_1"]))]
+
         if self.is_fmri_input is True:
             cond["c_concat"] = [input_mask * self.fmri2visual_model((xc["c_concat"])).detach()]
         else:
