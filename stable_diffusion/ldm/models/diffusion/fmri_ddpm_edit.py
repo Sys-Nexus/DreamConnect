7647070ad375b0c6f19eaeda46f2c87fb42050c0
sunyasheng <1044705955@qq.com>
2023-10-02 23:40:55 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
+++ stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
@@ -1341,7 +1341,7 @@ class DiffusionWrapper(nn.Module):
                 # cc_1 = torch.cat(c_crossattn_1, 1)
                 cc_1 = c_crossattn_1
             out = self.diffusion_model(xc, t, context=cc, context_1=cc_1)
-            import pdb; pdb.set_trace();
+            # import pdb; pdb.set_trace();
 
         elif self.conditioning_key == 'fmri_controlnet':
             xc = torch.cat([x] + [x], dim=1)
