f096b2a36b0090172223eabdd62d763779c79c9a
sunyasheng <1044705955@qq.com>
2023-10-02 22:56:06 +0400

    up

--- stable_diffusion/ldm/modules/diffusionmodules/openaimodel.py
+++ stable_diffusion/ldm/modules/diffusionmodules/openaimodel.py
@@ -791,7 +791,6 @@ class UNetModel(nn.Module):
         :param y: an [N] Tensor of labels, if class-conditional.
         :return: an [N x C x ...] Tensor of outputs.
         """
-        import pdb; pdb.set_trace();
         assert (y is not None) == (
             self.num_classes is not None
         ), "must specify y if and only if the model is class-conditional"
