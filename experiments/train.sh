9b94e1ec845ccfedcb1a88fc5199cfce4d259c59
sunyasheng <1044705955@qq.com>
2023-10-04 19:04:44 +0400

    up

--- experiments/train.sh
+++ experiments/train.sh
@@ -2,7 +2,9 @@
 # jobname=${1:-'fmri_instruct_encoder'}
 # jobname=${1:-'fmri_instruct_dual_condition'}
 # jobname=${1:-'fmri_instruct_controlnet'}
-jobname=${1:-'fmri_instruct_encoder_context'}
+# jobname=${1:-'fmri_instruct_encoder_context'}
+
+jobname=${1:-'fmri_reconstruct_instruct_diffusion'}
 mode=${2:-'single'}
 gpus=${3:-'3,'}
 master_port=${4:-'27198'}
