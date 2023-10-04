13d50164667a180edc25efaf163538ce004a3bfb
sunyasheng <1044705955@qq.com>
2023-10-04 23:34:14 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_ddpm_reconstruct.py
+++ stable_diffusion/ldm/models/diffusion/fmri_ddpm_reconstruct.py
@@ -702,16 +702,16 @@ class LatentDiffusion(DDPM):
         fmri_prompt_mask = rearrange(fmri_prompt_mask, "n -> n 1 1")
         input_mask = 1 - rearrange((random >= uncond*2).float() * (random < uncond*3).float(), "n -> n 1 1 1")
         
-        null_prompt = self.get_learned_conditioning([""], is_return_pool=True)[1]
+        null_prompt = self.get_learned_conditioning([""], is_return_pool=True)[1].unsqueeze(1)
         fmri_null_prompt = self.get_learned_conditioning_fmri(torch.zeros_like(xc["c_crossattn_1"]))
         fmri_learned_prompt = self.get_learned_conditioning_fmri(xc["c_crossattn_1"])        
 
         if force_c_encode is True:
-            cond["c_crossattn_1"] = fmri_learned_prompt
-            cond["c_crossattn"] = self.get_learned_conditioning(xc["c_crossattn"],is_return_pool=True)[1].detach()
+            cond["c_crossattn_1"] = [fmri_learned_prompt]
+            cond["c_crossattn"] = [self.get_learned_conditioning(xc["c_crossattn"],is_return_pool=True)[1].detach()]
         else:
             cond["c_crossattn_1"] = [torch.where(fmri_prompt_mask.bool(), fmri_null_prompt, fmri_learned_prompt)]
-            cond["c_crossattn"] = [torch.where(prompt_mask, null_prompt, self.get_learned_conditioning(xc["c_crossattn"], is_return_pool=True)[1].detach())]
+            cond["c_crossattn"] = [torch.where(prompt_mask, null_prompt, self.get_learned_conditioning(xc["c_crossattn"], is_return_pool=True)[1].detach()).unsqueeze(1)]
             # cond["c_crossattn_1"] = fmri_prompt_mask.float()*fmri_null_prompt + (1-fmri_prompt_mask.float())*fmri_learned_prompt
             # import pdb;pdb.set_trace()
 
