afe99a87c1604ab48052ba13a51505d08d94cabc
sunyasheng <1044705955@qq.com>
2023-10-04 09:21:52 +0400

    up

--- stable_diffusion/ldm/modules/encoders/fmri.py
+++ stable_diffusion/ldm/modules/encoders/fmri.py
@@ -36,7 +36,7 @@ class FmriEmbedder(nn.Module):
                                                   nn.ReLU(),
                                                   nn.Linear(768, 768)])
         self.adaptor_fmri2image_path = adaptor_fmri2image_path
-        import pdb; pdb.set_trace();
+        # import pdb; pdb.set_trace();
         if os.path.exists(adaptor_fmri2image_path):
             self.init_fmri_weight()
         # self.adaptor_fmri2image.eval()
