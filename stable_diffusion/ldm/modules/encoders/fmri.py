2341968e8a9ac795a60f81f0cc79c44a8df6cc71
sunyasheng <1044705955@qq.com>
2023-10-04 08:53:37 +0400

    up

--- stable_diffusion/ldm/modules/encoders/fmri.py
+++ stable_diffusion/ldm/modules/encoders/fmri.py
@@ -37,6 +37,7 @@ class FmriEmbedder(nn.Module):
                                                   nn.Linear(768, 768)])
         self.adaptor_fmri2image_path = adaptor_fmri2image_path
         if os.path.exists(adaptor_fmri2image_path):
+            import pdb; pdb.set_trace();
             self.init_fmri_weight()
         # self.adaptor_fmri2image.eval()
         # freeze(self.adaptor_fmri2image)
@@ -48,6 +49,7 @@ class FmriEmbedder(nn.Module):
         self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
 
     def init_fmri_weight(self):
+        import pdb; pdb.set_trace();
         state_dict = torch.load(self.adaptor_fmri2image_path)
         adaptor_state_dict = {k.replace('adaptor_fmri2image.', ''): v for k, v in state_dict['model'].items() if k.startswith('adaptor_fmri2image.')}
         self.adaptor_fmri2image.load_state_dict(adaptor_state_dict, strict=True)
