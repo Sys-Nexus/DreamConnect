13891f7a258260fa291cad4ddd0b2d2eab3326e2
sunyasheng <1044705955@qq.com>
2023-10-07 07:58:10 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_ddpm_reconstruct.py
+++ stable_diffusion/ldm/models/diffusion/fmri_ddpm_reconstruct.py
@@ -431,7 +431,11 @@ class LatentDiffusion(DDPM):
 
         if pretrained_unet_path is not None and os.path.exists(pretrained_unet_path):
             import pdb; pdb.set_trace();
-
+            pretrained_state_dict = torch.load(pretrained_unet_path, map_location="cpu")
+            missing, unexpected = self.model.diffusion_model.load_state_dict(pretrained_state_dict, strict=False)
+            print('missing: ', missing)
+            print('unexpected: ', unexpected)
+            
         self.cond_stage_forward_fmri = cond_stage_forward_fmri
         if fmri2visual_stage_config is not None: ## fmri to vae
             self.instantiate_fmri2visual_stage(fmri2visual_stage_config)
