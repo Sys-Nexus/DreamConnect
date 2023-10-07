243432241c45da3f48b0447a630b9b1395d4709c
sunyasheng <1044705955@qq.com>
2023-10-07 08:50:24 +0400

    up

--- third_party/CoDI/core/models/encoders/fmri.py
+++ third_party/CoDI/core/models/encoders/fmri.py
@@ -46,7 +46,7 @@ class FmriEmbedder(nn.Module):
         # self.adaptor_fmri2image = nn.Sequential(*[nn.Linear(1024, 768), nn.ReLU(), nn.Linear(768, 768)])
         # self.adaptor_fmri2image = nn.Linear(1024, 768)
         self.adaptor_fmri2image_path = adaptor_fmri2image_path
-        if not os.path.exist(self.adaptor_fmri2image_path): 
+        if not os.path.exists(self.adaptor_fmri2image_path): 
             print(self.adaptor_fmri2image_path, 'not exist.')
         else:
             self.init_fmri_weight()
