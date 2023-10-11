b6039ecb953f4641b73f79fb2dec265f2a99c5f5
sunyasheng <1044705955@qq.com>
2023-10-11 04:32:19 +0400

    up

--- experiments/train.sh
+++ experiments/train.sh
@@ -1,5 +1,6 @@
 # jobname=${1:-'fmri_reconstruct_codi'}
-jobname=${1:-'recon_tune_sideconv'}
+jobname=${1:-'instru_tune_sideconv'}
+# jobname=${1:-'recon_tune_sideconv'}
 filter_mode=${2:-'tune_sideconv'} #no_filter
 gpus=${3:-'3,'}
 master_port=${4:-'27198'}
