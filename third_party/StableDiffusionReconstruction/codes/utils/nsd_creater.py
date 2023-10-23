3c086e2cdca509c940a57f6b63acf5e6dd42804b
sunyasheng <1044705955@qq.com>
2023-10-23 18:39:45 +0400

    up

--- third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
+++ third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
@@ -74,7 +74,7 @@ def load_img_from_string(img_path,resolution):
 
 
 class NIPS23NSDDataset(Dataset):
-    def __init__(self, url="nsd_data_dir/webdataset_avg_split/metadata_subj01.json", voxels_key='nsdgeneral.npy', ):
+    def __init__(self, url="nsd_data_dir/test_subj01_" + "{0..1}.tar", voxels_key='nsdgeneral.npy', ):
         super().__init__()
         dl = wds.WebDataset(url, resampled=False)\
                 .decode("torch")\
