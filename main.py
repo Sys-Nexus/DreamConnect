802b3f4b648ad7d9424d0564ee7a53b567f28f83
sunyasheng <1044705955@qq.com>
2023-10-23 20:56:03 +0400

    up

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ from einops import rearrange
 
 third_party_proj_root = os.path.join(proj_root, 'third_party')
 sys.path.append(third_party_proj_root)
+sys.path.append('third_party/CoDI')
 
 class CFGDenoiser(nn.Module):
     def __init__(self, model):
