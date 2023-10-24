0e9815f4be06352b00e558f8dba7a277b314564b
sunyasheng <1044705955@qq.com>
2023-10-24 09:00:43 +0400

    up

--- experiments/train.sh
+++ experiments/train.sh
@@ -1,8 +1,11 @@
 # jobname=${1:-'fmri_reconstruct_codi'}
 # jobname=${1:-'instru_tune_sideconv'}
 
-jobname=${1:-'fmri_instruct_encoder_context'} # lower learning rate compared to instru_tune_sideconv
+jobname=${1:-'fmri_reconstruct_codi'} # lower learning rate compared to instru_tune_sideconv
 filter_mode=${2:-'no_filter'}
+
+# jobname=${1:-'fmri_instruct_encoder_context'} # lower learning rate compared to instru_tune_sideconv
+# filter_mode=${2:-'no_filter'}
 
 # jobname=${1:-'instru_tune_sideconv_sdunlock'} # lower learning rate compared to instru_tune_sideconv
 # filter_mode=${2:-'tune_sideconv_sdunlock'} #no_filter
@@ -12,7 +15,7 @@ filter_mode=${2:-'no_filter'}
 gpus=${3:-'3,'}
 master_port=${4:-'27198'}
 mode=${5:-'single'}
-vis=${6:-0}
+vis=${6:-1}
 
 
 num_nodes=1
