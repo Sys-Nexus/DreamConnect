839a9efccb891a34180a65efe4349e479574c9bb
sunyasheng <1044705955@qq.com>
2023-10-25 10:21:45 +0400

    up

--- third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
+++ third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
@@ -74,7 +74,7 @@ def load_img_from_string(img_path,resolution):
 
 
 class NIPS23NSDDataset(Dataset):
-    def __init__(self, url="nsd_data_dir/test_subj01_" + "{0..1}.tar", voxels_key='nsdgeneral.npy', split='train', resolution=320,\
+    def __init__(self, url="nsd_data_dir/test_subj01_" + "{0..1}.tar", voxels_key='nsdgeneral.npy', split='test', resolution=320,\
             nsd_root='/data/yashengsun/Proj/MMEdit/StableDiffusionReconstruction/nsd',
             is_reconstruct_mode=False,
             reconstruct_prob=0.1,):
