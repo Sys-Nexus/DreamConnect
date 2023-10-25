31aee680872ed988f2f95eb99f633b987cede6fe
sunyasheng <1044705955@qq.com>
2023-10-25 10:41:47 +0400

    up

--- third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
+++ third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
@@ -143,7 +143,7 @@ class NIPS23NSDDataset(Dataset):
                 instruction_text = random.choice(self.valid_do_nothing_ops)
                 nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
                 nsd_dict['edited'] = init_image[0]
-        print(nsd_dict['fmri_edit']['c_crossattn'])
+        print(nsd_dict['cap'], nsd_dict['fmri_edit']['c_crossattn'])
         return nsd_dict
 
     def __len__(self):
