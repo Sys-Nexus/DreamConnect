ca18616f8923f36f4ca943f61fbe4ee4f0058ccb
sunyasheng <1044705955@qq.com>
2023-10-25 11:02:14 +0400

    up

--- third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
+++ third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
@@ -140,13 +140,13 @@ class NIPS23NSDDataset(Dataset):
                 edited_path = os.path.join(self.edited_root, '{:06d}'.format(s), 'output_{:06d}_seed93151_id{}.jpg'.format(s,chosen_i))
                 nsd_dict['edited'] = load_img_from_string(edited_path, self.resolution)[0] # TODO
             except:
-                import tracebrack
-                tracebrack.print_exc()
+                # import tracebrack
+                # tracebrack.print_exc()
                 ## If the triplet pairs do not exist, use do nothing operation
                 instruction_text = random.choice(self.valid_do_nothing_ops)
                 nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
                 nsd_dict['edited'] = init_image[0]
-        print(nsd_dict['cap'], nsd_dict['fmri_edit']['c_crossattn'])
+        # print(nsd_dict['cap'], nsd_dict['fmri_edit']['c_crossattn'])
         return nsd_dict
 
     def __len__(self):
