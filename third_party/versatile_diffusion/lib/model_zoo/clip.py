b7517b49b41e365ee37b4847610ba90cc0bcbeaa
sunyasheng <1044705955@qq.com>
2023-11-03 10:59:17 +0400

    up

--- third_party/versatile_diffusion/lib/model_zoo/clip.py
+++ third_party/versatile_diffusion/lib/model_zoo/clip.py
@@ -65,6 +65,7 @@ class FrozenCLIP(AbstractEncoder):
         self.max_length = max_length  # TODO: typical value?
         self.encode_type = encode_type
         self.fp16 = fp16
+        import pdb; pdb.set_trace()
         self.freeze()
 
     def get_device(self):
