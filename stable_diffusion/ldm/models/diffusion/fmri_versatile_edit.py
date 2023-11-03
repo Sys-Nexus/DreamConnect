15d7897cd1ef7c66d3f4395a200682745a4c3833
sunyasheng <1044705955@qq.com>
2023-11-03 10:16:43 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_edit.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_edit.py
@@ -62,5 +62,78 @@ class fMRIVersatileEdit(LatentDiffusion):
 
         #net.model.cuda(1)
         sampler = sampler(net)
-    
-    # def 
+
+        n_samples = 1
+        ddim_steps = 50
+        ddim_eta = 0
+        scale = 7.5
+        strength = 0.75
+        mixing = 0.4
+        t_enc = int(strength * ddim_steps)
+
+        h, w = 512,512
+        shape = [n_samples, 4, h//8, w//8]
+
+        xtype = 'image'
+        ctype = 'prompt'
+        net.autokl.half()
+
+        sampler.model.model.diffusion_model.device='cuda:1'
+        sampler.model.model.diffusion_model.half().cuda(1)
+        
+        self.sampler = sampler
+        self.net = net
+        self.net.eval()
+
+        self.device = 'cuda:0'
+        self.t_enc = t_enc
+        self.ddim_steps = ddim_steps
+        self.ddim_eta = ddim_eta
+
+    @torch.no_grad()    
+    def log_images(self, batch, epoch_n, iter_n, batch_idx, model_wrap, model_wrap_cfg,
+                   save_dir, split,
+                   cfg_text=7.5, cfg_fmri=1.5,
+                   N=2, n_row=4, sample=True, 
+                   steps=100, ddim_eta=1., return_keys=None,
+                   quantize_denoised=True, inpaint=False):
+        # self.model.eval()
+        
+        # zim = Image.open('results/vdvae/subj{:02d}/{}.png'.format(sub,im_id))
+   
+        # zim = regularize_image(zim)
+        import pdb; pdb.set_trace();
+        zim = ...
+        zin = zim*2 - 1
+        zin = zin.unsqueeze(0).cuda(0).half()
+
+        init_latent = self.net.autokl_encode(zin)
+        
+        self.sampler.make_schedule(ddim_num_steps=self.ddim_steps, ddim_eta=self.ddim_eta, verbose=False)
+
+        dummy = ''
+        utx = self.net.clip_encode_text(dummy)
+        utx = utx.half()
+        
+        dummy = torch.zeros((1,3,224,224))
+        uim = self.net.clip_encode_vision(dummy)
+        uim = uim.half()
+        
+        cim = self.net.clip_encode_vision(zin)
+        ctx = self.net.clip_encode_text()
+
+        z_enc = self.sampler.stochastic_encode(init_latent, torch.tensor([self.t_enc]).to(self.device))
+        z = self.sampler.decode_dc(
+            x_latent=z_enc,
+            first_conditioning=[uim, cim],
+            second_conditioning=[utx, ctx],
+            t_start=t_enc,
+            unconditional_guidance_scale=scale,
+            xtype='image', 
+            first_ctype='vision',
+            second_ctype='prompt',
+            mixed_ratio=(1-mixing), )
+        
+        x = self.net.autokl_decode(z)
+        x = torch.clamp((x+1.0)/2.0, min=0.0, max=1.0)
+        import pdb; pdb.set_trace();
