043b89752e3ae01d03f43bdd688293d5def0d2f1
sunyasheng <1044705955@qq.com>
2023-11-04 19:17:35 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_edit.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_edit.py
@@ -8,6 +8,7 @@ import numpy as np
 import numpy.random as npr
 
 import torch
+import torch.nn as nn
 import torchvision.transforms as tvtrans
 from lib.cfg_helper import model_cfg_bank
 from lib.model_zoo import get_model
