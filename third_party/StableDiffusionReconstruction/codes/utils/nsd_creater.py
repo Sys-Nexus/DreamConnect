49c18b701ad60acf9f3a111e8bee675d7dc26911
sunyasheng <1044705955@qq.com>
2023-11-07 10:20:14 +0400

    up

--- third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
+++ third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
@@ -134,22 +134,25 @@ class NIPS23NSDDataset(Dataset):
             nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
             nsd_dict['edited'] = init_image[0]
         else:
-            try:
-                chosen_pool = []
-                for chosen_i in [0,1]:
-                    edited_path = os.path.join(self.edited_root, '{:06d}'.format(s), 'output_{:06d}_seed93151_id{}.jpg'.format(s,chosen_i))
-                    if os.path.exists(edited_path):
-                        chosen_pool.append(chosen_i)
-                chosen_i = random.choice(chosen_pool)
+            # try:
+            chosen_pool = []
+            for chosen_i in [0,1]:
+                edited_path = os.path.join(self.edited_root, '{:06d}'.format(s), 'output_{:06d}_seed93151_id{}.jpg'.format(s,chosen_i))
+                if os.path.exists(edited_path):
+                    chosen_pool.append(chosen_i)
+            chosen_i = random.choice(chosen_pool)
+            
+            if len(chosen_pool) and s in self.meta_info:
                 instruction_text = self.meta_info[s]['edit'][chosen_i]
                 nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
                 edited_path = os.path.join(self.edited_root, '{:06d}'.format(s), 'output_{:06d}_seed93151_id{}.jpg'.format(s,chosen_i))
                 nsd_dict['edited'] = load_img_from_string(edited_path, self.resolution)[0] # TODO
-            except:
-                import traceback
-                traceback.print_exc()
-                print('chosen : ', chosen_i, 's: ', s, 'cap: ', caps, 'instru: ', instruction_text)
+            # except:
+                # import traceback
+                # traceback.print_exc()
+                # print('chosen : ', chosen_i, 's: ', s, 'cap: ', caps, 'instru: ', instruction_text)
                 ## If the triplet pairs do not exist, use do nothing operation
+            else:
                 instruction_text = random.choice(self.valid_do_nothing_ops)
                 nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
                 nsd_dict['edited'] = init_image[0]
