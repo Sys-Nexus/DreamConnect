2d1fde40fceb0209dcf1f69b22d3bf7b7898d2a5
sunyasheng <1044705955@qq.com>
2023-11-07 09:30:58 +0400

    up

--- third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
+++ third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
@@ -135,14 +135,14 @@ class NIPS23NSDDataset(Dataset):
             nsd_dict['edited'] = init_image[0]
         else:
             try:
-                chosen_i = 0
+                chosen_i = random.choice([0,1])
                 instruction_text = self.meta_info[s]['edit'][chosen_i]
                 nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
                 edited_path = os.path.join(self.edited_root, '{:06d}'.format(s), 'output_{:06d}_seed93151_id{}.jpg'.format(s,chosen_i))
                 nsd_dict['edited'] = load_img_from_string(edited_path, self.resolution)[0] # TODO
             except:
-                # import tracebrack
-                # tracebrack.print_exc()
+                import tracebrack
+                tracebrack.print_exc()
                 ## If the triplet pairs do not exist, use do nothing operation
                 instruction_text = random.choice(self.valid_do_nothing_ops)
                 nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
@@ -153,111 +153,111 @@ class NIPS23NSDDataset(Dataset):
     def __len__(self):
         return len(self.cocos)
 
-class NSDDataset(Dataset):
-    ## it seems that ventral area is sensitive to captions
-    def __init__(self, nsd_root, idxes, batch_size=1, resolution=320, split='train', subject='subj01', 
-                is_reconstruct_mode=False,
-                reconstruct_prob=0.1,
-                # roi=['early', 'ventral', 'midventral', 'midlateral', 'lateral', 'parietal'], 
-                roi=['early'], 
-                target='context'):
-        self.nsda = NSDAccess(nsd_root)
-        self.idxes = idxes
-        self.reconstruct_prob=reconstruct_prob
-        self.batch_size = batch_size
-        self.resolution = resolution
-        self.split = split
-        self.is_reconstruct_mode = is_reconstruct_mode
+# class NSDDataset(Dataset):
+#     ## it seems that ventral area is sensitive to captions
+#     def __init__(self, nsd_root, idxes, batch_size=1, resolution=320, split='train', subject='subj01', 
+#                 is_reconstruct_mode=False,
+#                 reconstruct_prob=0.1,
+#                 # roi=['early', 'ventral', 'midventral', 'midlateral', 'lateral', 'parietal'], 
+#                 roi=['early'], 
+#                 target='context'):
+#         self.nsda = NSDAccess(nsd_root)
+#         self.idxes = idxes
+#         self.reconstruct_prob=reconstruct_prob
+#         self.batch_size = batch_size
+#         self.resolution = resolution
+#         self.split = split
+#         self.is_reconstruct_mode = is_reconstruct_mode
 
-        mridir = f'{os.path.dirname(nsd_root)}/mrifeat/{subject}/'
+#         mridir = f'{os.path.dirname(nsd_root)}/mrifeat/{subject}/'
 
-        X = []
-        X_te = []
-        for croi in roi:
-            if 'conv' in target: # We use averaged features for GAN due to large number of dimension of features
-                cX = np.load(f'{mridir}/{subject}_{croi}_betas_ave_tr.npy').astype("float32")
-            else:
-                cX = np.load(f'{mridir}/{subject}_{croi}_betas_tr.npy').astype("float32")
-            cX_te = np.load(f'{mridir}/{subject}_{croi}_betas_ave_te.npy').astype("float32")
-            # print(split, target)
-            # import pdb; pdb.set_trace();
-            X.append(cX)
-            X_te.append(cX_te)
-        X = np.hstack(X)
-        X_te = np.hstack(X_te)
-        self.X = X
-        self.X_te = X_te
+#         X = []
+#         X_te = []
+#         for croi in roi:
+#             if 'conv' in target: # We use averaged features for GAN due to large number of dimension of features
+#                 cX = np.load(f'{mridir}/{subject}_{croi}_betas_ave_tr.npy').astype("float32")
+#             else:
+#                 cX = np.load(f'{mridir}/{subject}_{croi}_betas_tr.npy').astype("float32")
+#             cX_te = np.load(f'{mridir}/{subject}_{croi}_betas_ave_te.npy').astype("float32")
+#             # print(split, target)
+#             # import pdb; pdb.set_trace();
+#             X.append(cX)
+#             X_te.append(cX_te)
+#         X = np.hstack(X)
+#         X_te = np.hstack(X_te)
+#         self.X = X
+#         self.X_te = X_te
 
-        nsd_root = os.path.dirname(os.path.abspath(__file__))
-        stats_path = os.path.join(nsd_root, 'misc/stats_{}.pkl'.format('_'.join(roi)+'_'+target))
-        if split == 'train' and not os.path.exists(stats_path):
-            self.X_mean, self.X_std = X.mean(axis=0,keepdims=True), X.std(axis=0,keepdims=True)
-            stats = {'X_mean': self.X_mean, 'X_std': self.X_std}
-            stats_save_pickle(stats, stats_path)
-        else:
-            stats = stats_load_pickle(stats_path)
-            self.X_mean, self.X_std = stats['X_mean'], stats['X_std']
-        # print('X shape: ', X.shape)
-        # import pdb; pdb.set_trace()
+#         nsd_root = os.path.dirname(os.path.abspath(__file__))
+#         stats_path = os.path.join(nsd_root, 'misc/stats_{}.pkl'.format('_'.join(roi)+'_'+target))
+#         if split == 'train' and not os.path.exists(stats_path):
+#             self.X_mean, self.X_std = X.mean(axis=0,keepdims=True), X.std(axis=0,keepdims=True)
+#             stats = {'X_mean': self.X_mean, 'X_std': self.X_std}
+#             stats_save_pickle(stats, stats_path)
+#         else:
+#             stats = stats_load_pickle(stats_path)
+#             self.X_mean, self.X_std = stats['X_mean'], stats['X_std']
+#         # print('X shape: ', X.shape)
+#         # import pdb; pdb.set_trace()
 
-        # vae_root = '/data/yashengsun/Proj/MMEdit/StableDiffusionReconstruction/nsdfeat_256/init_latent'
-        # self.vae_paths = {s: os.path.join(vae_root, '{:06d}.npy'.format(s)) for s in self.idxes}
+#         # vae_root = '/data/yashengsun/Proj/MMEdit/StableDiffusionReconstruction/nsdfeat_256/init_latent'
+#         # self.vae_paths = {s: os.path.join(vae_root, '{:06d}.npy'.format(s)) for s in self.idxes}
 
-        nsd_coco_caption_path = os.path.join(nsd_root, 'misc/nsd_coco_caption.pkl')
-        self.caps, self.keys, self.cap_dict = read_pkl(nsd_coco_caption_path)
-        # import pdb; pdb.set_trace();
+#         nsd_coco_caption_path = os.path.join(nsd_root, 'misc/nsd_coco_caption.pkl')
+#         self.caps, self.keys, self.cap_dict = read_pkl(nsd_coco_caption_path)
+#         # import pdb; pdb.set_trace();
 
-        self.meta_info = read_edit_json(os.path.join(nsd_root, 'misc'), self.keys)
-        self.edited_root = '/data/yashengsun/Proj/Diffusion/InstructDiffusion/nsd_coco_output'
+#         self.meta_info = read_edit_json(os.path.join(nsd_root, 'misc'), self.keys)
+#         self.edited_root = '/data/yashengsun/Proj/Diffusion/InstructDiffusion/nsd_coco_output'
 
-        self.valid_do_nothing_ops = [' ']
+#         self.valid_do_nothing_ops = [' ']
 
-    def __getitem__(self, index):
-        s = self.idxes[index]
-        # m_idx = self.mri_idxes[index]
-        # prompt = []
-        # prompts = self.nsda.read_image_coco_info([s], info_type='captions')
-        # for p in prompts:
-        #     prompt.append(p['caption'])
+#     def __getitem__(self, index):
+#         s = self.idxes[index]
+#         # m_idx = self.mri_idxes[index]
+#         # prompt = []
+#         # prompts = self.nsda.read_image_coco_info([s], info_type='captions')
+#         # for p in prompts:
+#         #     prompt.append(p['caption'])
 
-        caps = self.cap_dict[s]
+#         caps = self.cap_dict[s]
 
-        img = self.nsda.read_images(s)
-        init_image = load_img_from_arr(img, self.resolution)
-        init_image = repeat(init_image, '1 ... -> b ...', b=self.batch_size)
+#         img = self.nsda.read_images(s)
+#         init_image = load_img_from_arr(img, self.resolution)
+#         init_image = repeat(init_image, '1 ... -> b ...', b=self.batch_size)
 
-        fmri_norm = (self.X[index]-self.X_mean)/self.X_std
-        ## TODO: use random.choices()
-        # nsd_dict = {'cap': caps[0], 'prompt': prompt[0],  'image': init_image[0], 'fmri': fmri_norm}
-        nsd_dict = {'cap': random.choices(caps)[0], 'image': init_image[0], 'fmri': fmri_norm, 's': s}
+#         fmri_norm = (self.X[index]-self.X_mean)/self.X_std
+#         ## TODO: use random.choices()
+#         # nsd_dict = {'cap': caps[0], 'prompt': prompt[0],  'image': init_image[0], 'fmri': fmri_norm}
+#         nsd_dict = {'cap': random.choices(caps)[0], 'image': init_image[0], 'fmri': fmri_norm, 's': s}
 
-        # image_vae = np.load(self.vae_paths[s])
-        # nsd_dict['image_vae'] = image_vae
+#         # image_vae = np.load(self.vae_paths[s])
+#         # nsd_dict['image_vae'] = image_vae
 
-        # TODO: currently , only use the first edit instruction
+#         # TODO: currently , only use the first edit instruction
 
-        if self.is_reconstruct_mode or random.uniform(0,1.)<self.reconstruct_prob:
-            instruction_text = random.choice(self.valid_do_nothing_ops)
-            nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
-            # nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': random.choices(caps)[0], 'c_crossattn_1': fmri_norm}
-            nsd_dict['edited'] = init_image[0]
-        else:
-            try:
-                chosen_i = 0
-                instruction_text = self.meta_info[s]['edit'][chosen_i]
-                nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
-                edited_path = os.path.join(self.edited_root, '{:06d}'.format(s), 'output_{:06d}_seed93151_id{}.jpg'.format(s,chosen_i))
-                nsd_dict['edited'] = load_img_from_string(edited_path, self.resolution)[0] # TODO
-            except:
-                ## If the triplet pairs do not exist, use do nothing operation
-                instruction_text = random.choice(self.valid_do_nothing_ops)
-                nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
-                nsd_dict['edited'] = init_image[0]
-        return nsd_dict
+#         if self.is_reconstruct_mode or random.uniform(0,1.)<self.reconstruct_prob:
+#             instruction_text = random.choice(self.valid_do_nothing_ops)
+#             nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
+#             # nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': random.choices(caps)[0], 'c_crossattn_1': fmri_norm}
+#             nsd_dict['edited'] = init_image[0]
+#         else:
+#             try:
+#                 chosen_i = random.choice([0,1])
+#                 instruction_text = self.meta_info[s]['edit'][chosen_i]
+#                 nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
+#                 edited_path = os.path.join(self.edited_root, '{:06d}'.format(s), 'output_{:06d}_seed93151_id{}.jpg'.format(s,chosen_i))
+#                 nsd_dict['edited'] = load_img_from_string(edited_path, self.resolution)[0] # TODO
+#             except:
+#                 ## If the triplet pairs do not exist, use do nothing operation
+#                 instruction_text = random.choice(self.valid_do_nothing_ops)
+#                 nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
+#                 nsd_dict['edited'] = init_image[0]
+#         return nsd_dict
 
-    def __len__(self):
-        if self.split == 'train': return self.X.shape[0]
-        else: return self.X_te.shape[0]
+#     def __len__(self):
+#         if self.split == 'train': return self.X.shape[0]
+#         else: return self.X_te.shape[0]
 
 
 
