cc087515b450f30e994d44f9635b0f4b5359f15a
sunyasheng <1044705955@qq.com>
2023-11-09 21:35:36 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_edit.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_edit.py
@@ -160,8 +160,9 @@ class ControlLDM(LatentDiffusion):
         
         with torch.no_grad():
             # import pdb; pdb.set_trace()
-            voxel = batch['fmri'].to(z).mean(dim=1)
-            print(voxel.shape)
+            voxel = batch['fmri'].to(z)
+            if voxel.shape[2] == 3: voxel = voxel.mean(dim=1)
+            # print(voxel.shape)
             self.fmri2lowlevel = self.fmri2lowlevel.float()
             lowlevel_vae = self.fmri2lowlevel(voxel).half()
 
