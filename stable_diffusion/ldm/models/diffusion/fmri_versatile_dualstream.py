988cdd7ebacc8429db310dd335ce5b8f52a30fcf
sunyasheng <1044705955@qq.com>
2023-11-12 11:49:20 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -78,6 +78,7 @@ class VersatileNetAdaptor(UNetModelVD):
         for i_module, t_module, zero_conv in zip(self.unet_image.output_blocks, self.unet_text.output_blocks, self.zero_convs):
             h = th.cat([h, hs.pop()], dim=1)
             h = self.mixed_run_dc(i_module, t_module, h, emb, c0, c1, xtype, c0_type, c1_type, mixed_ratio)
+            print(h.shape)
             outs.append(zero_conv(h, emb))
 
         if xtype == 'image':
