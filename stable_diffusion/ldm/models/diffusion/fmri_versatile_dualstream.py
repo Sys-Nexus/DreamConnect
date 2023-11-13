2fab9795b7889499e6ca1852ab073fe760a7b706
sunyasheng <1044705955@qq.com>
2023-11-13 16:12:39 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -247,8 +247,8 @@ class DualLDM(LatentDiffusion):
                    quantize_denoised=True, inpaint=False):
         x_gt, c = self.get_input(batch, self.first_stage_key, force_c_encode=True)
         # import pdb; pdb.set_trace();
-        init_latent = torch.cat(c["c_crossattn_1"]["fmri_vae"],dim=0)
-        # init_latent = self.first_stage_model.encode(batch['image'].half().to(x_gt.device)).mode()
+        # init_latent = torch.cat(c["c_crossattn_1"]["fmri_vae"],dim=0)
+        init_latent = self.first_stage_model.encode(batch['image'].half().to(x_gt.device)).mode()
 
         self.device = x_gt.device
         self.sampler.model.model.diffusion_model.device = x_gt.device
