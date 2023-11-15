432ec589a09269b2340eb485c0a2d0b04b1c8383
sunyasheng <1044705955@qq.com>
2023-11-15 10:07:20 +0400

    up

---
+++ third_party/fMRI_reconstruction_NSD/src/models.py
@@ -0,0 +1,78 @@
+import os
+import numpy as np
+from torchvision import transforms
+import torch
+import torch.nn as nn
+import PIL
+import clip
+import open_clip
+from functools import partial
+
+# for prior
+# from dalle2_pytorch import DiffusionPrior
+# from dalle2_pytorch.dalle2_pytorch import l2norm, default, exists
+from tqdm.auto import tqdm
+import random
+import json
+
+
+class BrainNetwork(nn.Module):
+    def __init__(self, out_dim=768, in_dim=15724, clip_size=768, h=4096, n_blocks=4, norm_type='ln', act_first=False, use_projector=True):
+        super().__init__()
+        norm_func = partial(nn.BatchNorm1d, num_features=h) if norm_type == 'bn' else partial(nn.LayerNorm, normalized_shape=h)
+        act_fn = partial(nn.ReLU, inplace=True) if norm_type == 'bn' else nn.GELU
+        act_and_norm = (act_fn, norm_func) if act_first else (norm_func, act_fn)
+        # self.temp = nn.Parameter(torch.tensor(.006))
+        self.lin0 = nn.Sequential(
+            nn.Linear(in_dim, h),
+            *[item() for item in act_and_norm],
+            nn.Dropout(0.5),
+        )
+        self.mlp = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(h, h),
+                *[item() for item in act_and_norm],
+                nn.Dropout(0.15)
+            ) for _ in range(n_blocks)
+        ])
+        self.lin1 = nn.Linear(h, out_dim, bias=True)
+        self.n_blocks = n_blocks
+        self.clip_size = clip_size
+        
+        self.use_projector = use_projector
+        if use_projector:
+            self.projector = nn.Sequential(
+                nn.LayerNorm(clip_size),
+                nn.GELU(),
+                nn.Linear(clip_size, 2048),
+                nn.LayerNorm(2048),
+                nn.GELU(),
+                nn.Linear(2048, 2048),
+                nn.LayerNorm(2048),
+                nn.GELU(),
+                nn.Linear(2048, clip_size)
+            )
+        
+    def forward(self, x):
+        '''
+            bs, 1, 15724 -> bs, 32, h
+            bs, 32, h -> bs, 32h
+            b2, 32h -> bs, 768
+        '''
+        if x.ndim == 4:
+            # case when we passed 3D data of shape [N, 81, 104, 83]
+            assert x.shape[1] == 81 and x.shape[2] == 104 and x.shape[3] == 83
+            # [N, 699192]
+            x = x.reshape(x.shape[0], -1)
+
+        x = self.lin0(x)  # bs, h
+        residual = x
+        for res_block in range(self.n_blocks):
+            x = self.mlp[res_block](x)
+            x += residual
+            residual = x
+        x = x.reshape(len(x), -1)
+        x = self.lin1(x)
+        if self.use_projector:
+            return x, self.projector(x.reshape(len(x), -1, self.clip_size))
+        return x
