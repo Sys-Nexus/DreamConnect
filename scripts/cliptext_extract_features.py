5d855df2a83531280fa41f0e1475d92a029b35b7
sunyasheng <1044705955@qq.com>
2023-11-16 14:17:35 +0400

    up

--- scripts/cliptext_extract_features.py
+++ scripts/cliptext_extract_features.py
@@ -16,19 +16,6 @@ from third_party.StableDiffusionReconstruction.codes.utils.nsd_creater import NI
 
 def main():
     sub = 1
-    # train_caps = np.load('data/processed_data/subj{:02d}/nsd_train_cap_sub{}.npy'.format(sub,sub)) 
-    # test_caps = np.load('data/processed_data/subj{:02d}/nsd_test_cap_sub{}.npy'.format(sub,sub))  
-
-    # cfgm_name = 'vd_noema'
-    # pth = 'versatile_diffusion/pretrained/vd-four-flow-v1-0-fp16-deprecated.pth'
-    # cfgm = model_cfg_bank()(cfgm_name)
-    # net = get_model()(cfgm)
-
-    # sd = torch.load(pth, map_location='cpu')
-    # net.load_state_dict(sd, strict=False)  
-    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # device = torch.device('cpu')
-    # net.clip = net.clip.to(device)
     
     # params copied from configs/versatile_dualstream.yaml
     clip_params = {'symbol':'clip', 'args':{}, 'name':'clip_frozen', 'type':'clip_frozen'}
@@ -62,22 +49,41 @@ def main():
 
     os.makedirs('nsd_data_dir/extracted_features/subj{:02d}'.format(sub), exist_ok=True)
     # import pdb; pdb.set_trace()
-    with torch.no_grad():
-        # for i in tqdm(range(num_test)):
-        #     cin = [test_dataset[i]['cap']]
-        #     c = vd_clip.clip_encode_text(cin)
-        #     test_clip[i] = c.to('cpu').numpy().mean(0)
+    # with torch.no_grad():
+    #     for i in tqdm(range(num_test)):
+    #         cin = [test_dataset[i]['cap']]
+    #         c = vd_clip.clip_encode_text(cin)
+    #         test_clip[i] = c.to('cpu').numpy().mean(0)
         
-        # np.save('nsd_data_dir/extracted_features/subj{:02d}/nsd_cliptext_test.npy'.format(sub),test_clip)
+    #     np.save('nsd_data_dir/extracted_features/subj{:02d}/nsd_cliptext_test.npy'.format(sub),test_clip)
             
-        for i in tqdm(range(num_train)):
-            try:
-                cin = [train_dataset[i]['cap']]
-                c = vd_clip.clip_encode_text(cin)
-                train_clip[i] = c.to('cpu').numpy().mean(0)
-            except:
-                print(i, cin)
-        np.save('nsd_data_dir/extracted_features/subj{:02d}/nsd_cliptext_train.npy'.format(sub),train_clip)
+    #     for i in tqdm(range(num_train)):
+    #         cin = [train_dataset[i]['cap']]
+    #         c = vd_clip.clip_encode_text(cin)
+    #         train_clip[i] = c.to('cpu').numpy().mean(0)
+        
+    #     np.save('nsd_data_dir/extracted_features/subj{:02d}/nsd_cliptext_train.npy'.format(sub),train_clip)
+
+    os.makedirs('nsd_data_dir/processed_data/subj{:02d}'.format(sub), exist_ok=True)
+    train_fmri_path = 'nsd_data_dir/processed_data/subj{:02d}/nsd_train_fmriavg_nsdgeneral_sub{}.npy'.format(sub,sub)
+    test_fmri_path = 'nsd_data_dir/processed_data/subj{:02d}/nsd_test_fmriavg_nsdgeneral_sub{}.npy'.format(sub,sub)
+
+    test_fmri, train_fmri = [], []
+    for i in tqdm(range(num_test)):
+        test_fmri_i = test_dataset[i]['fmri']
+        import pdb; pdb.set_trace();
+        test_fmri.append(test_fmri_i)
+
+    test_fmri = np.concatenate(test_fmri)
+    np.save(test_fmri_path, test_fmri)
+
+    for i in tqdm(range(num_train)):
+        train_fmri_i = train_dataset[i]['fmri']
+        train_fmri.append(train_fmri_i)
+
+    train_fmri = np.concatenate(train_fmri)
+    np.save(train_fmri_path, train_fmri)
+
 
 
 if __name__ == '__main__':
---
+++ scripts/cliptext_regression.py
@@ -0,0 +1,90 @@
+import sys
+import numpy as np
+import sklearn.linear_model as skl
+import pickle
+import argparse
+import torch
+import numpy as np
+import os
+import sys
+from easydict import EasyDict
+from tqdm import tqdm
+
+proj_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(proj_root)
+sys.path.append(os.path.join(proj_root, "stable_diffusion"))
+sys.path.append(os.path.join(proj_root, 'third_party/versatile_diffusion'))
+
+from lib.model_zoo.vd import VDCLIP
+from third_party.StableDiffusionReconstruction.codes.utils.nsd_creater import NIPS23NSDDataset
+
+
+def main():
+    sub = 1
+
+    train_ds_params = {
+        'nsd_root': '/data/yashengsun/Proj/MMEdit/StableDiffusionReconstruction/nsd',
+        'resolution': 320,
+        'split': 'train',
+        'is_reconstruct_mode': False,
+        'url': 'nsd_data_dir/train_subj01_{0..17}.tar',
+        'reconstruct_prob': 1.01,
+    }
+    test_ds_params = {
+        'nsd_root': '/data/yashengsun/Proj/MMEdit/StableDiffusionReconstruction/nsd',
+        'resolution': 320,
+        'split': 'test',
+        'is_reconstruct_mode': False,
+        'url': 'nsd_data_dir/test_subj01_{0..1}.tar',
+        'reconstruct_prob': 1.01,
+    }
+    
+    train_dataset = NIPS23NSDDataset(**train_ds_params)
+    test_dataset = NIPS23NSDDataset(**test_ds_params)
+
+
+    #### TODO: add preprocessing normalization.
+    num_voxels, num_train, num_test = train_fmri.shape[1], len(train_fmri), len(test_fmri)
+
+    train_clip = np.load('nsd_data_dir/extracted_features/subj{:02d}/nsd_cliptext_train.npy'.format(sub))
+    test_clip = np.load('nsd_data_dir/extracted_features/subj{:02d}/nsd_cliptext_test.npy'.format(sub))
+
+    train_path = 'nsd_data_dir/processed_data/subj{:02d}/nsd_train_fmriavg_nsdgeneral_sub{}.npy'.format(sub,sub)
+    train_fmri = np.load(train_path)
+    test_path = 'nsd_data_dir/processed_data/subj{:02d}/nsd_test_fmriavg_nsdgeneral_sub{}.npy'.format(sub,sub)
+    test_fmri = np.load(test_path)
+
+    ## Regression
+    num_samples,num_embed,num_dim = train_clip.shape
+
+    print("Training Regression")
+    reg_w = np.zeros((num_embed,num_dim,num_voxels)).astype(np.float32)
+    reg_b = np.zeros((num_embed,num_dim)).astype(np.float32)
+    pred_clip = np.zeros_like(test_clip)
+    
+    for i in range(num_embed):
+        reg = skl.Ridge(alpha=100000, max_iter=50000, fit_intercept=True)
+        reg.fit(train_fmri, train_clip[:,i])
+        reg_w[i] = reg.coef_
+        reg_b[i] = reg.intercept_
+        
+        pred_test_latent = reg.predict(test_fmri)
+        std_norm_test_latent = (pred_test_latent - np.mean(pred_test_latent,axis=0)) / np.std(pred_test_latent,axis=0)
+        pred_clip[:,i] = std_norm_test_latent * np.std(train_clip[:,i],axis=0) + np.mean(train_clip[:,i],axis=0)
+        print(i,reg.score(test_fmri,test_clip[:,i]))
+
+    np.save('data/predicted_features/subj{:02d}/nsd_cliptext_predtest_nsdgeneral.npy'.format(sub),pred_clip)
+
+
+    datadict = {
+        'weight' : reg_w,
+        'bias' : reg_b,
+
+    }
+
+    with open('data/regression_weights/subj{:02d}/cliptext_regression_weights.pkl'.format(sub),"wb") as f:
+    pickle.dump(datadict,f)
+
+
+if __name__ == '__main__':
+    main()
