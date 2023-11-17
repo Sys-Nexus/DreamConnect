ee2960c9554d309cfe642478624f87c1c0d18d0f
sunyasheng <1044705955@qq.com>
2023-11-17 07:26:05 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -241,10 +241,10 @@ class DualLDM(LatentDiffusion):
 
         fmri_null_x = self.vd_clip.clip_encode_vision(null_x)
         fmri_null_cap = self.vd_clip.clip_encode_text(null_cap)
-        # fmri_x = batch['nsd_clipvision'][:null_x.shape[0]].to(fmri_null_x)
-        # fmri_cap = batch['nsd_cliptext'][:null_x.shape[0]].to(fmri_null_x)
-        fmri_x = self.vd_clip.clip_encode_vision(x)
-        fmri_cap = self.vd_clip.clip_encode_text(cap)
+        fmri_x = batch['nsd_clipvision'][:null_x.shape[0]].to(fmri_null_x)
+        fmri_cap = batch['nsd_cliptext'][:null_x.shape[0]].to(fmri_null_x)
+        # fmri_x = self.vd_clip.clip_encode_vision(x)
+        # fmri_cap = self.vd_clip.clip_encode_text(cap)
 
 
         cond["c_crossattn_1"] = {}
