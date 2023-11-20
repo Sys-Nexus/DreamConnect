0ea4cb0ada2565312c3786cc2f68acc2cd544b47
sunyasheng <1044705955@qq.com>
2023-11-20 10:37:46 +0400

    up

--- third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
+++ third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
@@ -156,7 +156,7 @@ class NIPS23NSDDataset(Dataset):
                 if os.path.exists(edited_path):
                     chosen_pool.append(chosen_i)
 
-            if len(chosen_pool) and s in self.meta_info and 'edit' in self.meta_info[s]:
+            if len(chosen_pool) and s in self.meta_info and 'edit' in self.meta_info[s] and max(chosen_pool) < len(self.meta_info[s]['edit']):
                 chosen_i = random.choice(chosen_pool)
                 instruction_text = self.meta_info[s]['edit'][chosen_i]
                 nsd_dict['fmri_edit'] = {'c_concat': init_image[0], 'c_crossattn': instruction_text, 'c_crossattn_1': fmri_norm}
