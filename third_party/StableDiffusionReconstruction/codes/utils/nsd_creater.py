0effc150c48a3798ce4184bfbd2391eb9ce05e5d
sunyasheng <1044705955@qq.com>
2023-11-20 10:30:52 +0400

    up

--- third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
+++ third_party/StableDiffusionReconstruction/codes/utils/nsd_creater.py
@@ -51,7 +51,7 @@ def read_edit_json(root, map_keys):
     # for i in range(len(meta)):
     for i in range(len(map_keys)):
         res_dict[map_keys[i]] = meta[i]
-    import pdb; pdb.set_trace();
+    # import pdb; pdb.set_trace();
     return res_dict
 
 
