c3a7ff16a881e464c7f24ea2e88dc87dbe8b14f1
sunyasheng <1044705955@qq.com>
2023-11-21 11:28:31 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -153,6 +153,7 @@ class DualLDM(LatentDiffusion):
         self.sampler = DDIMSampler_Dual(self)
 
         ddim_steps = 50
+        ddim_steps = 500
         ddim_eta = 0
         scale = 7.5
         strength = 0.75
