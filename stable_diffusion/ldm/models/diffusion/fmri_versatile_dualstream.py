31cdc35cf28ca30fe23c048f18481ac5896c5b31
sunyasheng <1044705955@qq.com>
2023-11-26 20:23:52 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -342,8 +342,8 @@ class DualLDM(LatentDiffusion):
         sigmas_std = model_wrap.get_sigmas(steps_std)
         z_pred_std = torch.randn_like(z_enc)
         print('prompt_emb: ', prompt_emb.shape, 'null_prompt_emb:', null_prompt_emb.shape)
-        cond_std = {"c_crossattn": [prompt_emb]}
-        uncond_std = {"c_crossattn": [null_prompt_emb]}
+        cond_std = {"c_crossattn": [prompt_emb], "c_concat": c['c_concat']}
+        uncond_std = {"c_crossattn": [null_prompt_emb], "c_concat": torch.zeros_like(c['c_concat'])}
         extra_args = {
             "cond": cond_std,
             "uncond": uncond_std,
