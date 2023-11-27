9fdea80c71153e5d1d00144b0c318edff6ee7df1
sunyasheng <1044705955@qq.com>
2023-11-27 11:18:45 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -342,7 +342,7 @@ class DualLDM(LatentDiffusion):
         save_path = os.path.join("debug", "images", "new",  
                 "all_iter-{:06}_ep-{:06}_bidx-{:06d}-{:06d}-{}.png".format(iter_n, epoch_n, batch_idx, s, instruct_cap))
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
-        torchvision.utils.save_image(x_pred_std*0.5+0.5, save_path)
+        torchvision.utils.save_image(x_edit*0.5+0.5, save_path)
 
         ######### another way to sampling ############
         steps_std = 50
