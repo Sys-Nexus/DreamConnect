83cfd467325545c4e3eeb4153f453c000da9a7f6
sunyasheng <1044705955@qq.com>
2023-11-27 09:08:21 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -377,126 +377,126 @@ class DualLDM(LatentDiffusion):
         self.model.train()
         # import pdb; pdb.set_trace()
 
-    def apply_model(self, x_noisy_gen, x_noisy_edit, t, cond=None, return_ids=False):
-        # import pdb; pdb.set_trace()
-        # if cond is None:
-        #     return super().apply_model(x_noisy=x_noisy_gen, t=x_noisy_edit, 
-        #                                      cond=t, return_ids=cond)
-        if isinstance(cond, dict):
-            # hybrid case, cond is exptected to be a dict
-            pass
-        else:
-            if not isinstance(cond, list):
-                cond = [cond]
-            key = 'c_concat' if self.model.conditioning_key == 'concat' else 'c_crossattn'
-            cond = {key: cond}
+    # def apply_model(self, x_noisy_gen, x_noisy_edit, t, cond=None, return_ids=False):
+    #     # import pdb; pdb.set_trace()
+    #     # if cond is None:
+    #     #     return super().apply_model(x_noisy=x_noisy_gen, t=x_noisy_edit, 
+    #     #                                      cond=t, return_ids=cond)
+    #     if isinstance(cond, dict):
+    #         # hybrid case, cond is exptected to be a dict
+    #         pass
+    #     else:
+    #         if not isinstance(cond, list):
+    #             cond = [cond]
+    #         key = 'c_concat' if self.model.conditioning_key == 'concat' else 'c_crossattn'
+    #         cond = {key: cond}
         
-        if hasattr(self, "split_input_params"):
-            assert len(cond) == 1  # todo can only deal with one conditioning atm
-            assert not return_ids
-            ks = self.split_input_params["ks"]  # eg. (128, 128)
-            stride = self.split_input_params["stride"]  # eg. (64, 64)
+    #     if hasattr(self, "split_input_params"):
+    #         assert len(cond) == 1  # todo can only deal with one conditioning atm
+    #         assert not return_ids
+    #         ks = self.split_input_params["ks"]  # eg. (128, 128)
+    #         stride = self.split_input_params["stride"]  # eg. (64, 64)
 
-            h, w = x_noisy.shape[-2:]
+    #         h, w = x_noisy.shape[-2:]
 
-            fold, unfold, normalization, weighting = self.get_fold_unfold(x_noisy, ks, stride)
+    #         fold, unfold, normalization, weighting = self.get_fold_unfold(x_noisy, ks, stride)
 
-            z = unfold(x_noisy)  # (bn, nc * prod(**ks), L)
-            # Reshape to img shape
-            z = z.view((z.shape[0], -1, ks[0], ks[1], z.shape[-1]))  # (bn, nc, ks[0], ks[1], L )
-            z_list = [z[:, :, :, :, i] for i in range(z.shape[-1])]
+    #         z = unfold(x_noisy)  # (bn, nc * prod(**ks), L)
+    #         # Reshape to img shape
+    #         z = z.view((z.shape[0], -1, ks[0], ks[1], z.shape[-1]))  # (bn, nc, ks[0], ks[1], L )
+    #         z_list = [z[:, :, :, :, i] for i in range(z.shape[-1])]
 
-            if self.cond_stage_key in ["image", "LR_image", "segmentation",
-                                       'bbox_img'] and self.model.conditioning_key:  # todo check for completeness
-                c_key = next(iter(cond.keys()))  # get key
-                c = next(iter(cond.values()))  # get value
-                assert (len(c) == 1)  # todo extend to list with more than one elem
-                c = c[0]  # get element
+    #         if self.cond_stage_key in ["image", "LR_image", "segmentation",
+    #                                    'bbox_img'] and self.model.conditioning_key:  # todo check for completeness
+    #             c_key = next(iter(cond.keys()))  # get key
+    #             c = next(iter(cond.values()))  # get value
+    #             assert (len(c) == 1)  # todo extend to list with more than one elem
+    #             c = c[0]  # get element
 
-                c = unfold(c)
-                c = c.view((c.shape[0], -1, ks[0], ks[1], c.shape[-1]))  # (bn, nc, ks[0], ks[1], L )
+    #             c = unfold(c)
+    #             c = c.view((c.shape[0], -1, ks[0], ks[1], c.shape[-1]))  # (bn, nc, ks[0], ks[1], L )
 
-                cond_list = [{c_key: [c[:, :, :, :, i]]} for i in range(c.shape[-1])]
+    #             cond_list = [{c_key: [c[:, :, :, :, i]]} for i in range(c.shape[-1])]
 
-            elif self.cond_stage_key == 'coordinates_bbox':
-                assert 'original_image_size' in self.split_input_params, 'BoudingBoxRescaling is missing original_image_size'
+    #         elif self.cond_stage_key == 'coordinates_bbox':
+    #             assert 'original_image_size' in self.split_input_params, 'BoudingBoxRescaling is missing original_image_size'
 
-                # assuming padding of unfold is always 0 and its dilation is always 1
-                n_patches_per_row = int((w - ks[0]) / stride[0] + 1)
-                full_img_h, full_img_w = self.split_input_params['original_image_size']
-                # as we are operating on latents, we need the factor from the original image size to the
-                # spatial latent size to properly rescale the crops for regenerating the bbox annotations
-                num_downs = self.first_stage_model.encoder.num_resolutions - 1
-                rescale_latent = 2 ** (num_downs)
+    #             # assuming padding of unfold is always 0 and its dilation is always 1
+    #             n_patches_per_row = int((w - ks[0]) / stride[0] + 1)
+    #             full_img_h, full_img_w = self.split_input_params['original_image_size']
+    #             # as we are operating on latents, we need the factor from the original image size to the
+    #             # spatial latent size to properly rescale the crops for regenerating the bbox annotations
+    #             num_downs = self.first_stage_model.encoder.num_resolutions - 1
+    #             rescale_latent = 2 ** (num_downs)
 
-                # get top left postions of patches as conforming for the bbbox tokenizer, therefore we
-                # need to rescale the tl patch coordinates to be in between (0,1)
-                tl_patch_coordinates = [(rescale_latent * stride[0] * (patch_nr % n_patches_per_row) / full_img_w,
-                                         rescale_latent * stride[1] * (patch_nr // n_patches_per_row) / full_img_h)
-                                        for patch_nr in range(z.shape[-1])]
+    #             # get top left postions of patches as conforming for the bbbox tokenizer, therefore we
+    #             # need to rescale the tl patch coordinates to be in between (0,1)
+    #             tl_patch_coordinates = [(rescale_latent * stride[0] * (patch_nr % n_patches_per_row) / full_img_w,
+    #                                      rescale_latent * stride[1] * (patch_nr // n_patches_per_row) / full_img_h)
+    #                                     for patch_nr in range(z.shape[-1])]
 
-                # patch_limits are tl_coord, width and height coordinates as (x_tl, y_tl, h, w)
-                patch_limits = [(x_tl, y_tl,
-                                 rescale_latent * ks[0] / full_img_w,
-                                 rescale_latent * ks[1] / full_img_h) for x_tl, y_tl in tl_patch_coordinates]
-                # patch_values = [(np.arange(x_tl,min(x_tl+ks, 1.)),np.arange(y_tl,min(y_tl+ks, 1.))) for x_tl, y_tl in tl_patch_coordinates]
+    #             # patch_limits are tl_coord, width and height coordinates as (x_tl, y_tl, h, w)
+    #             patch_limits = [(x_tl, y_tl,
+    #                              rescale_latent * ks[0] / full_img_w,
+    #                              rescale_latent * ks[1] / full_img_h) for x_tl, y_tl in tl_patch_coordinates]
+    #             # patch_values = [(np.arange(x_tl,min(x_tl+ks, 1.)),np.arange(y_tl,min(y_tl+ks, 1.))) for x_tl, y_tl in tl_patch_coordinates]
 
-                # tokenize crop coordinates for the bounding boxes of the respective patches
-                patch_limits_tknzd = [torch.LongTensor(self.bbox_tokenizer._crop_encoder(bbox))[None]
-                                      for bbox in patch_limits]  # list of length l with tensors of shape (1, 2)
-                print(patch_limits_tknzd[0].shape)
-                # cut tknzd crop position from conditioning
-                assert isinstance(cond, dict), 'cond must be dict to be fed into model'
-                cut_cond = cond['c_crossattn'][0][..., :-2]
-                print(cut_cond.shape)
+    #             # tokenize crop coordinates for the bounding boxes of the respective patches
+    #             patch_limits_tknzd = [torch.LongTensor(self.bbox_tokenizer._crop_encoder(bbox))[None]
+    #                                   for bbox in patch_limits]  # list of length l with tensors of shape (1, 2)
+    #             print(patch_limits_tknzd[0].shape)
+    #             # cut tknzd crop position from conditioning
+    #             assert isinstance(cond, dict), 'cond must be dict to be fed into model'
+    #             cut_cond = cond['c_crossattn'][0][..., :-2]
+    #             print(cut_cond.shape)
 
-                adapted_cond = torch.stack([torch.cat([cut_cond, p], dim=1) for p in patch_limits_tknzd])
-                adapted_cond = rearrange(adapted_cond, 'l b n -> (l b) n')
-                print(adapted_cond.shape)
-                adapted_cond = self.get_learned_conditioning(adapted_cond)
-                print(adapted_cond.shape)
-                adapted_cond = rearrange(adapted_cond, '(l b) n d -> l b n d', l=z.shape[-1])
-                print(adapted_cond.shape)
+    #             adapted_cond = torch.stack([torch.cat([cut_cond, p], dim=1) for p in patch_limits_tknzd])
+    #             adapted_cond = rearrange(adapted_cond, 'l b n -> (l b) n')
+    #             print(adapted_cond.shape)
+    #             adapted_cond = self.get_learned_conditioning(adapted_cond)
+    #             print(adapted_cond.shape)
+    #             adapted_cond = rearrange(adapted_cond, '(l b) n d -> l b n d', l=z.shape[-1])
+    #             print(adapted_cond.shape)
 
-                cond_list = [{'c_crossattn': [e]} for e in adapted_cond]
+    #             cond_list = [{'c_crossattn': [e]} for e in adapted_cond]
 
-            else:
-                cond_list = [cond for i in range(z.shape[-1])]  # Todo make this more efficient
+    #         else:
+    #             cond_list = [cond for i in range(z.shape[-1])]  # Todo make this more efficient
 
-            # apply model by loop over crops
-            output_list = [self.model(z_list[i], t, **cond_list[i]) for i in range(z.shape[-1])]
-            assert not isinstance(output_list[0],
-                                  tuple)  # todo cant deal with multiple model outputs check this never happens
+    #         # apply model by loop over crops
+    #         output_list = [self.model(z_list[i], t, **cond_list[i]) for i in range(z.shape[-1])]
+    #         assert not isinstance(output_list[0],
+    #                               tuple)  # todo cant deal with multiple model outputs check this never happens
 
-            o = torch.stack(output_list, axis=-1)
-            o = o * weighting
-            # Reverse reshape to img shape
-            o = o.view((o.shape[0], -1, o.shape[-1]))  # (bn, nc * ks[0] * ks[1], L)
-            # stitch crops together
-            x_recon = fold(o) / normalization
+    #         o = torch.stack(output_list, axis=-1)
+    #         o = o * weighting
+    #         # Reverse reshape to img shape
+    #         o = o.view((o.shape[0], -1, o.shape[-1]))  # (bn, nc * ks[0] * ks[1], L)
+    #         # stitch crops together
+    #         x_recon = fold(o) / normalization
 
-        else:
-            ## only add this
-            new_cond = copy.deepcopy(cond)
-            new_cond["only_mid_control"] = self.only_mid_control
-            c0 = torch.cat(new_cond["c_crossattn_1"]["image_emb"], 1)
-            c1 = torch.cat(new_cond["c_crossattn_1"]["text_emb"], 1)
-            fmri_vae = torch.cat(new_cond["c_crossattn_1"]["fmri_vae"],1)
+    #     else:
+    #         ## only add this
+    #         new_cond = copy.deepcopy(cond)
+    #         new_cond["only_mid_control"] = self.only_mid_control
+    #         c0 = torch.cat(new_cond["c_crossattn_1"]["image_emb"], 1)
+    #         c1 = torch.cat(new_cond["c_crossattn_1"]["text_emb"], 1)
+    #         fmri_vae = torch.cat(new_cond["c_crossattn_1"]["fmri_vae"],1)
 
-            # import pdb; pdb.set_trace()
-            x_recon_gen, control_res = self.control_model.forward_dc(x=torch.cat([x_noisy_gen], dim=1), 
-                                                        # hint=fmri_vae,
-                                                        timesteps=t,
-                                                        c0=c0, c1=c1,
-                                                        xtype='image', c0_type='vision', 
-                                                        c1_type='prompt', mixed_ratio=0.6)
-            new_cond.pop('c_crossattn_1')
-            new_cond.pop('null_prompt_emb')
-            fmri_control = [c * scale for c, scale in zip(control_res, self.control_scales)]
-            # new_cond["control"] = fmri_control
-            new_cond["control"] = None
-            ## only add above
-            # import pdb; pdb.set_trace()
-            x_recon_edit = self.model(x_noisy_edit, t, **new_cond)
+    #         # import pdb; pdb.set_trace()
+    #         x_recon_gen, control_res = self.control_model.forward_dc(x=torch.cat([x_noisy_gen], dim=1), 
+    #                                                     # hint=fmri_vae,
+    #                                                     timesteps=t,
+    #                                                     c0=c0, c1=c1,
+    #                                                     xtype='image', c0_type='vision', 
+    #                                                     c1_type='prompt', mixed_ratio=0.6)
+    #         new_cond.pop('c_crossattn_1')
+    #         new_cond.pop('null_prompt_emb')
+    #         fmri_control = [c * scale for c, scale in zip(control_res, self.control_scales)]
+    #         # new_cond["control"] = fmri_control
+    #         new_cond["control"] = None
+    #         ## only add above
+    #         # import pdb; pdb.set_trace()
+    #         x_recon_edit = self.model(x_noisy_edit, t, **new_cond)
 
-        return x_recon_gen, x_recon_edit
+    #     return x_recon_gen, x_recon_edit
