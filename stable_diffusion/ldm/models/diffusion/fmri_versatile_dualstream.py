a8d4180eb1ee086d69365fb8c9ce25d8090aa1c6
sunyasheng <1044705955@qq.com>
2023-11-28 15:27:37 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -61,10 +61,6 @@ class ControlledUnetModel(UNetModel):
             if control is not None:
                 h += control.pop(0)
 
-            # for control_i, h_i in zip(control[::-1], hs):
-            #     print(control_i.shape, h_i.shape)
-            # import pdb; pdb.set_trace()
-
             for i, module in enumerate(self.output_blocks):
                 # print(i, hs[-1].shape, control[0].shape)
                 # if i in unmatched_layers:
@@ -76,7 +72,7 @@ class ControlledUnetModel(UNetModel):
                     # print(h.shape, hs[-1].shape)
                     h = torch.cat([h, hs.pop()], dim=1)
                 else:
-                    print('insert: ', i, hs[-1].shape, control[0].shape)
+                    # print('insert: ', i, hs[-1].shape, control[0].shape)
                     h = torch.cat([h, hs.pop() + control.pop(0)], dim=1)
                 h = module(h, emb, context)
 
@@ -206,7 +202,7 @@ class PreVersatileNetAdaptor(UNetModelVD):
             out_i = zero_conv(h, emb)
             outs.append(out_i)
             hs.append(h)
-            print('gen: ', i, h.shape, out_i.shape)
+            # print('gen: ', i, h.shape, out_i.shape)
 
         h = self.mixed_run_dc(
             self.unet_image.middle_block, self.unet_text.middle_block, 
