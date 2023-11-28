b0412c9c051caedfa510ec88f8bb7134b6ca7348
sunyasheng <1044705955@qq.com>
2023-11-28 11:02:54 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -101,19 +101,23 @@ class PostVersatileNetAdaptor(UNetModelVD):
         self.middle_block_out = self.make_zero_conv(ch)
 
         unmatched_layers = [2, 5, 8]
+        out_cs = {2: None, 5: 640, 8: None}
         stride_i = 0
         for level_idx, mult in list(enumerate(channel_mult))[::-1]:
             for block_idx in range(self.num_noattn_blocks[level_idx] + 1):
                 ch = mult * model_channels
                 # print('ch: ', ch)
                 if stride_i in unmatched_layers:
-                    self.zero_convs.append(self.make_zero_conv(ch, kernel_size=2, stride=2))
+                    self.zero_convs.append(self.make_zero_conv(ch, kernel_size=2, stride=2, out_channels=out_cs[stride_i]))
                 else:
                     self.zero_convs.append(self.make_zero_conv(ch))
                 stride_i += 1
 
-    def make_zero_conv(self, channels, kernel_size=1, stride=1):
-        return TimestepEmbedSequential(zero_module(conv_nd(self.dims, channels, channels, kernel_size, stride=stride, padding=0)))
+    def make_zero_conv(self, channels, kernel_size=1, stride=1, out_channels=None):
+        if out_channels is None:
+            return TimestepEmbedSequential(zero_module(conv_nd(self.dims, channels, channels, kernel_size, stride=stride, padding=0)))
+        else:
+            return TimestepEmbedSequential(zero_module(conv_nd(self.dims, channels, out_channels, kernel_size, stride=stride, padding=0)))
 
     def forward_dc(self, x, timesteps, c0, c1, xtype, c0_type, c1_type, mixed_ratio):
         # print(x.shape, c0.shape, c1.shape, timesteps)
