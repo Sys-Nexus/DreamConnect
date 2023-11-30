30b310f8c499dda65e2a0743db24580b3d1de332
sunyasheng <1044705955@qq.com>
2023-11-30 20:08:42 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -90,6 +90,10 @@ class FusionPriorUnetModel(UNetModel):
                 ch = out_ch
                 ds *= 2
 
+        layers = [SpatialTransformer(ch, num_heads, dim_head, default_eps=default_eps, force_type_convert=force_type_convert, 
+                                                depth=transformer_depth, context_dim=context_dim)]
+                    self.merge_blocks.append(TimestepEmbedSequential(*layers))
+
         for level, mult in list(enumerate(channel_mult))[::-1]:
             for i in range(num_res_blocks + 1):
                 ch = model_channels * mult
