9a22a6abd5c20ad69f3ea714ef936595c0123d47
sunyasheng <1044705955@qq.com>
2023-11-30 21:37:34 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -147,7 +147,9 @@ class FusionPriorUnetModel(UNetModel):
                 else:
                     # h = torch.cat([h, hs.pop() + control.pop(0)], dim=1)
                     import pdb; pdb.set_trace()
-                    h = torch.cat([h, self.merge_blocks[i+1](torch.cat([hs.pop(),control.pop(0)],dim=-1), emb)[:,:,:,:h.shape[-1]]], dim=1)
+                    print(hs[-1].shape, control[0].shape, h.shape)
+                    mix = self.merge_blocks[i+1](torch.cat([hs.pop(),control.pop(0)],dim=-1), emb)[:,:,:,:h.shape[-1]]
+                    h = torch.cat([h, mix], dim=1)
                 h = module(h, emb, context)
 
             # import pdb; pdb.set_trace()
