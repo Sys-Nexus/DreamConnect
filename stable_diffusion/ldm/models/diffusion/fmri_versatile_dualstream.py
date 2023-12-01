3aa8393def3502fbba8853d344cfa4b4f9f31e66
sunyasheng <1044705955@qq.com>
2023-12-01 09:18:40 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -56,6 +56,10 @@ class FusionPriorUnetModel(UNetModel):
         model_channels = self.model_channels
         num_heads = self.num_heads
         legacy = self.legacy
+        
+        print(self.transformer_depth)
+        import pdb; pdb.set_trace()
+        transformer_depth = 1
 
         self.merge_blocks = nn.ModuleList([])
         merge_blocks_list = []
