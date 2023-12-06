5765d76da892b4782771db332f1a8e04f0ec8e98
sunyasheng <1044705955@qq.com>
2023-12-06 16:57:39 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_codi_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_codi_dualstream.py
@@ -197,7 +197,7 @@ class DualLDM(LatentDiffusion):
 
         pretrained_codi_clip_path = kwargs['pretrained_codi_clip_path']
         if pretrained_codi_clip_path is not None and os.path.exists(pretrained_codi_clip_path):
-            import pdb; pdb.set_trace()
+            # import pdb; pdb.set_trace()
             pretrained_state_dict = torch.load(pretrained_codi_clip_path, map_location="cpu")
             pretrained_state_dict = {k:v for k,v in pretrained_state_dict.items() if 'clip.' in k}
             missing, unexpected = self.codi_clip.load_state_dict(pretrained_state_dict, strict=False)
