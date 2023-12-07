87f566a06355eb77d8a3d5eed0fb7d7273a3c459
sunyasheng <1044705955@qq.com>
2023-12-07 21:45:54 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -496,7 +496,8 @@ class DualLDM(LatentDiffusion):
         #     cond["c_concat"] = [input_mask * self.fmri2visual_model((xc["c_concat"])).detach()]
         # else:
             # cond["c_concat"] = [input_mask * self.encode_first_stage((xc["c_concat"])).mode().detach()]
-        c_concat = F.interpolate(xc["c_concat"], (sz,sz)) if sz is not None else xc["c_concat"]
+        # c_concat = F.interpolate(xc["c_concat"], (sz,sz)) if sz is not None else xc["c_concat"]
+        c_concat = F.interpolate(xc["c_concat"], (x.shape[-1],x.shape[-1]))
         cond["c_concat"] = [self.encode_first_stage(c_concat).mode().detach()]
         out = [z, cond]
         if return_first_stage_outputs:
