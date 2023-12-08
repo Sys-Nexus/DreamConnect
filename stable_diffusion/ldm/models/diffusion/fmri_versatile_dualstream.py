af440038e6e3b717e4c14151e07c42e3e1f53181
sunyasheng <1044705955@qq.com>
2023-12-08 16:10:48 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -732,18 +732,19 @@ class DualLDM(LatentDiffusion):
             fmri_vae = torch.cat(new_cond["c_crossattn_1"]["fmri_vae"],1)
 
             # import pdb; pdb.set_trace()
-            # with torch.no_grad():
-            x_recon_gen, control_res = self.control_model.forward_dc(x=torch.cat([x_noisy_gen], dim=1), 
-                                                        # hint=fmri_vae,
-                                                        timesteps=t,
-                                                        c0=c0, c1=c1,
-                                                        xtype='image', c0_type='vision', 
-                                                        c1_type='prompt', mixed_ratio=0.6)
+            with torch.no_grad():
+                x_recon_gen, control_res = self.control_model.forward_dc(x=torch.cat([x_noisy_gen], dim=1), 
+                                                            # hint=fmri_vae,
+                                                            timesteps=t,
+                                                            c0=c0, c1=c1,
+                                                            xtype='image', c0_type='vision', 
+                                                            c1_type='prompt', mixed_ratio=0.6)
             # control_res = [tt.detach().requires_grad_(True) for tt in control_res]
             new_cond.pop('c_crossattn_1')
             new_cond.pop('null_prompt_emb')
             fmri_control = [c * scale for c, scale in zip(control_res, self.control_scales)]
             new_cond["control"] = fmri_control
+            x_recon_gen = x_recon_gen.requires_grad_(True)
             new_cond["noisy_c_concat"] = [x_recon_gen]
             # new_cond["control"] = None
             ## only add above
