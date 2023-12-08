8bb30167b670514a95f80c70833916e3c271778f
sunyasheng <1044705955@qq.com>
2023-12-08 09:49:51 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -542,19 +542,19 @@ class DualLDM(LatentDiffusion):
         cfg_text_edit, cfg_image_edit = 7.5, 2.5
 
         if cfg_text_edit is not None and cfg_image_edit is not None:
-            c_w_uncond["c_crossattn_1"]["image_emb"] = [torch.cat([uncond_c0, c0], 0)]
-            c_w_uncond["c_crossattn_1"]["text_emb"] = [torch.cat([uncond_c1, c1], 0)]
-            c_w_uncond["c_crossattn"] = [torch.cat([null_prompt_emb, prompt_emb], 0)]
-            c_concat = F.interpolate(xc["c_concat"], (512, 512))
-            c_concat = self.encode_first_stage(c_concat).mode().detach()
-            c_w_uncond["c_concat"] = [torch.cat([c_concat,]*2, 0)]
-        else:
             c_w_uncond["c_crossattn_1"]["image_emb"] = [torch.cat([uncond_c0, c0, c0], 0)]
             c_w_uncond["c_crossattn_1"]["text_emb"] = [torch.cat([uncond_c1, c1, c1], 0)]
             c_w_uncond["c_crossattn"] = [torch.cat([null_prompt_emb, prompt_emb, prompt_emb], 0)]
             c_concat = F.interpolate(xc["c_concat"], (512, 512))
             c_concat = self.encode_first_stage(c_concat).mode().detach()
             c_w_uncond["c_concat"] = [torch.cat([c_concat, torch.zeros_like(c_concat), c_concat], 0)]
+        else:
+            c_w_uncond["c_crossattn_1"]["image_emb"] = [torch.cat([uncond_c0, c0], 0)]
+            c_w_uncond["c_crossattn_1"]["text_emb"] = [torch.cat([uncond_c1, c1], 0)]
+            c_w_uncond["c_crossattn"] = [torch.cat([null_prompt_emb, prompt_emb], 0)]
+            c_concat = F.interpolate(xc["c_concat"], (512, 512))
+            c_concat = self.encode_first_stage(c_concat).mode().detach()
+            c_w_uncond["c_concat"] = [torch.cat([c_concat,]*2, 0)]            
 
         # z_enc = self.sampler.stochastic_encode(init_latent, torch.tensor([self.t_enc]).to(z_gt.device))
         # z_enc_gen = z_enc_edit = z_enc
