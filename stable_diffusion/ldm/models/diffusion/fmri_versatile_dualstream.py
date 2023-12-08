a54b4cc63dfb4a956fceab11d23ca1f44dee6277
sunyasheng <1044705955@qq.com>
2023-12-08 08:29:21 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -551,7 +551,7 @@ class DualLDM(LatentDiffusion):
 
         z_enc_gen = self.sampler.stochastic_encode(init_latent, torch.tensor([int(0.75*50)]).to(z_gt.device))
         z_enc_edit = torch.randn_like(init_latent) * self.scale_factor
-        t_enc = 50
+        t_enc = 37
 
         instruct_cap = batch['fmri_edit']['c_crossattn'][0]
         ######### designed dual-stream diffusion sampling ##########
