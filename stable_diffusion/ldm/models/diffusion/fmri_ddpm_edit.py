24e92835ff457a930cafc806a68d62fa729af51f
sunyasheng <1044705955@qq.com>
2023-12-08 22:43:33 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
+++ stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
@@ -1445,7 +1445,10 @@ class DiffusionWrapper(nn.Module):
             cc = torch.cat(c_crossattn, 1)
             out = self.diffusion_model(xc, t, context=cc, control=control, only_mid_control=only_mid_control)
         elif self.conditioning_key == 'fmri_noisy_concat':
-            xc = torch.cat([x] + noisy_c_concat, dim=1)
+            if noisy_c_concat is None:
+                xc = torch.cat([x] + [x], dim=1)
+            else:
+                xc = torch.cat([x] + noisy_c_concat, dim=1)
             cc = torch.cat(c_crossattn, 1)
             out = self.diffusion_model(xc, t, context=cc, control=control, only_mid_control=only_mid_control)
         elif self.conditioning_key == 'adm':
