b5b86f9f00ba76365b5ec8955f91a9ee3b8edfa2
sunyasheng <1044705955@qq.com>
2023-12-10 09:49:12 +0400

    up

--- third_party/versatile_diffusion/lib/model_zoo/ddim_vd_dual.py
+++ third_party/versatile_diffusion/lib/model_zoo/ddim_vd_dual.py
@@ -595,7 +595,7 @@ class DDIMSampler_Dual(DDIMSampler):
             index = total_steps - i - 1
             ts = torch.full((x_latent_edit.shape[0],), step, device=x_latent_edit.device, dtype=torch.long)
             if unconditional_guidance_scale_edit is None:
-                cond_dict['noisy_c_concat'] = torch.cat([x0_dec_gen]*3, dim=0) #/ 0.18215
+                cond_dict['noisy_c_concat'] = torch.cat([x0_dec_gen]*3, dim=0) / 0.18215
                 x_dec_gen, x0_dec_gen, x_dec_edit, x0_dec_edit = self.p_sample_ddim_dual_cfg(
                     x_dec_gen, 
                     x_dec_edit,
