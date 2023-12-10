53d5dd8e640c71e9d897006eae73892146aa8d11
sunyasheng <1044705955@qq.com>
2023-12-10 07:58:26 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -785,9 +785,9 @@ class DualLDM(LatentDiffusion):
             ## only add above
             # import pdb; pdb.set_trace()
             # print('timesteps: ', t)
-            # edit_t = t if delay_t is None else t + delay_t
-            # edit_t = torch.clamp(edit_t, max=961)
-            edit_t = t
+            edit_t = t if delay_t is None else t + delay_t
+            edit_t = torch.clamp(edit_t, max=961)
+            # edit_t = t
             x_recon_edit = self.model(x_noisy_edit, edit_t, **new_cond)
 
         return x_recon_gen, x_recon_edit
