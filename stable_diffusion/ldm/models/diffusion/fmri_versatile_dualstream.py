2e1531fe788b40eead0ba7b9270273eabca9d3a0
sunyasheng <1044705955@qq.com>
2023-12-14 16:55:53 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -280,9 +280,10 @@ class PreVersatileNetAdaptor(UNetModelVD):
         final_out = self.unet_image.out(h)
         
         if is_save_x0 is True:
-            denoised_x0 = (x0 - sqrt_one_minus_at * final_out) / a_t.sqrt() / 0.18215
+            denoised_x0 = (x0 - sqrt_one_minus_at * final_out) / a_t.sqrt()
+            denoised_x0_fix = (self.x0_block_out(denoised_x0, emb) + denoised_x0) / 0.18215
             # outs.append(self.x0_block_out(denoised_x0, emb))
-            outs.append(denoised_x0)
+            outs.append(denoised_x0_fix)
             # print('denoised shape: ', denoised_x0.shape)
             # import pdb; pdb.set_trace();
 
