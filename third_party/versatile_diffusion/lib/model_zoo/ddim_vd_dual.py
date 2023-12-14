9c02ec332198f9296d89c4c007b7100577775ede
sunyasheng <1044705955@qq.com>
2023-12-14 12:33:15 +0400

    up

--- third_party/versatile_diffusion/lib/model_zoo/ddim_vd_dual.py
+++ third_party/versatile_diffusion/lib/model_zoo/ddim_vd_dual.py
@@ -668,7 +668,7 @@ class DDIMSampler_Dual(DDIMSampler):
             ts = torch.full((x_latent_edit.shape[0],), step, device=x_latent_edit.device, dtype=torch.long)
             a_t, sqrt_one_minus_at = self.get_al(x_dec_gen, index)
 
-            # import pdb; pdb.set_trace()
+            import pdb; pdb.set_trace()
             x_dec_gen, x0_dec_gen, x_dec_edit, x0_dec_edit = self.p_sample_ddim_dual_cfg(
                 x_dec_gen, 
                 x_dec_edit,
