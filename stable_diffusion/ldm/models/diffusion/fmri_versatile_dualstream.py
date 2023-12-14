55222b0ce2137a4c0751541618e7fe62dfb1880a
sunyasheng <1044705955@qq.com>
2023-12-14 06:38:09 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -185,6 +185,7 @@ class ControlledUnetModel(UNetModel):
     def forward(self, x, timesteps=None, context=None, control=None, only_mid_control=False, num_control_layers=1000, **kwargs):
         # print(x.shape, timesteps)
         if control is not None:
+            control.pop(0)
             # unmatched_layers = [2, 5, 8]
             hs = []
             with torch.no_grad():
