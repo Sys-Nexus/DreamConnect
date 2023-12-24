b959707d551bfad5244135d104f208ffd7983bb6
sunyasheng <1044705955@qq.com>
2023-12-24 22:52:52 +0400

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -823,8 +823,8 @@ class DualLDM(LatentDiffusion):
             new_cond.pop('null_prompt_emb')
             
             noisy_c_concat = control_res.pop(0)
-            fmri_control = [c * scale for c, scale in zip(control_res, self.control_scales)]
-            new_cond["control"] = fmri_control
+            # fmri_control = [c * scale for c, scale in zip(control_res, self.control_scales)]
+            # new_cond["control"] = fmri_control
 
             out_layers_injected = {f"output_block_4_out_layers_features": control_res.pop(0)}
             new_cond["injected_features"] = out_layers_injected
