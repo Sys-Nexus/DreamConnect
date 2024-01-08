6345e81fd13905eed92570a6e703b6956bcc003a
sunyasheng <1044705955@qq.com>
2024-01-08 15:18:20 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -140,15 +140,14 @@ class PreVersatileNetAdaptor(UNetModelVD):
 
         for i, (i_module, t_module, zero_conv) in enumerate(zip(self.unet_image.input_blocks, self.unet_text.input_blocks, self.zero_convs)):
             h = self.mixed_run_dc(i_module, t_module, h, emb, c0, c1, xtype, c0_type, c1_type, mixed_ratio)
-            if is_save_intermediate is True:
-                outs.append(zero_conv(h, emb))
+            # if is_save_intermediate is True:
+            #     outs.append(zero_conv(h, emb))
             hs.append(h)
 
-        h = self.mixed_run_dc(
-            self.unet_image.middle_block, self.unet_text.middle_block, 
-            h, emb, c0, c1, xtype, c0_type, c1_type, mixed_ratio)
-        if is_save_intermediate is True:
-            outs.append(self.middle_block_out(h, emb))
+        h = self.mixed_run_dc(self.unet_image.middle_block, self.unet_text.middle_block, 
+                                h, emb, c0, c1, xtype, c0_type, c1_type, mixed_ratio)
+        # if is_save_intermediate is True:
+        #     outs.append(self.middle_block_out(h, emb))
 
         block_idx = 0
         for i_module, t_module in zip(self.unet_image.output_blocks, self.unet_text.output_blocks):
