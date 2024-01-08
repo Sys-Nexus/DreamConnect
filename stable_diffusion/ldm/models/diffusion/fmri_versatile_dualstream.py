007b22260160afe79f1cf6d55d22c53a4a26f72c
sunyasheng <1044705955@qq.com>
2024-01-08 09:21:22 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -230,7 +230,7 @@ class DualLDM(LatentDiffusion):
             print('vox2clip vclip: [unexpected] ', len(unexpected))
     
     ### TODO: what we should give to noise_edit
-    def p_losses(self, x_start_gen, x_start_edit, cond, t, noise=None, noise_edit=None, t_edit=None, is_return_x0=False):
+    def p_losses(self, x_start_gen, x_start_edit, cond, t, noise=None, noise_edit=None, t_edit=None, is_return_x0=True):
         # import pdb; pdb.set_trace();
         noise_gen = default(noise, lambda: torch.randn_like(x_start_gen))
         x_noisy_gen = self.q_sample(x_start=x_start_gen, t=t, noise=noise_gen)
