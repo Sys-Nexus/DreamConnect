36122b8d9f5aeb2acd16442604603e5502e0a4d8
sunyasheng <1044705955@qq.com>
2024-01-08 16:26:38 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -163,7 +163,7 @@ class PreVersatileNetAdaptor(UNetModelVD):
 
         block_idx = 0
         for i, (i_module, t_module, zero_conv) in enumerate(zip(self.unet_image.output_blocks, self.unet_text.output_blocks, self.zero_convs)):
-            print('i: ', i)
+            # print('i: ', i)
             h = th.cat([h, hs.pop()], dim=1)
             h = self.mixed_run_dc(i_module, t_module, h, emb, c0, c1, xtype, c0_type, c1_type, mixed_ratio)
             if block_idx in useful_block_idxes:
