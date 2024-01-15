34d9a37cbd1866291c646a7ea864daadefc5ba26
sunyasheng <1044705955@qq.com>
2024-01-15 21:37:22 +0900

    up

--- main.py
+++ main.py
@@ -510,8 +510,8 @@ def train_one_epoch(config, model, model_ema, data_loader, val_data_loader, opti
                         if model_wrap is not None:
                             model.log_images(batch, epoch, idx, val_idx, model_wrap, model_wrap_cfg, save_dir, 'val', cfg_text=cfg_text, cfg_fmri=2.5)
 
-                        if val_idx == 5:
-                            break
+                        # if val_idx == 5:
+                        #     break
                 else:
                     for val_idx, batch in enumerate(data_loader):
                         batch_size = batch['image'].shape[0]
