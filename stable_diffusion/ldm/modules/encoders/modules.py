6d0d74e2955852d8fa77e1bb35890657314f7dd3
sunyasheng <1044705955@qq.com>
2024-03-03 00:21:01 +0900

    up

--- stable_diffusion/ldm/modules/encoders/modules.py
+++ stable_diffusion/ldm/modules/encoders/modules.py
@@ -134,6 +134,204 @@ class SpatialRescaler(nn.Module):
     def encode(self, x):
         return self(x)
 
+
+class ExFrozenCLIPEmbedder(AbstractEncoder):
+    """Uses the CLIP transformer encoder for text (from Hugging Face)"""
+    def __init__(self, version="openai/clip-vit-large-patch14", device="cuda", max_length=77):
+        super().__init__()
+        self.tokenizer = CLIPTokenizer.from_pretrained(version)
+        self.transformer = CLIPTextModel.from_pretrained(version)
+
+        self.device = device
+        self.max_length = max_length
+        self.freeze()
+
+        def embedding_forward(
+                self,
+                input_ids = None,
+                position_ids = None,
+                inputs_embeds = None,
+                embedding_manager = None,
+                prospect_words = None,
+            ) -> list:
+                # import pdb; pdb.set_trace()
+                seq_length = input_ids.shape[-1] if input_ids is not None else inputs_embeds.shape[-2]
+
+                if position_ids is None:
+                    position_ids = self.position_ids[:, :seq_length]
+
+                if inputs_embeds is None:
+                    # print('CLIP->embedding_forward->inputs_embeds=None')
+                    inputs_embeds = self.token_embedding(input_ids)
+
+                if embedding_manager is not None:
+                    # print('CLIP->embedding_forward->embedding_manager is not None')
+                    inputs_embeds = embedding_manager(input_ids, inputs_embeds, prospect_words = prospect_words)
+
+                    position_embeddings = self.position_embedding(position_ids)
+                    embeddings = []
+                    for i in range(len(inputs_embeds)):
+                        embeddings.append(inputs_embeds[i] + position_embeddings)
+
+                    return embeddings  
+                        
+                position_embeddings = self.position_embedding(position_ids)
+                embeddings = inputs_embeds + position_embeddings
+                # import pdb; pdb.set_trace()
+                return embeddings      
+
+        self.transformer.text_model.embeddings.forward = embedding_forward.__get__(self.transformer.text_model.embeddings)
+
+        def encoder_forward(
+            self,
+            inputs_embeds,
+            attention_mask = None,
+            causal_attention_mask = None,
+            output_attentions = None,
+            output_hidden_states = None,
+            return_dict = None,
+        ):
+            output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+            output_hidden_states = (
+                output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+            )
+            return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+            encoder_states = () if output_hidden_states else None
+            all_attentions = () if output_attentions else None
+
+            hidden_states = inputs_embeds
+            for idx, encoder_layer in enumerate(self.layers):
+                print('self.layers', idx, encoder_layer)
+                # import pdb; pdb.set_trace()
+                if output_hidden_states:
+                    encoder_states = encoder_states + (hidden_states,)
+
+                layer_outputs = encoder_layer(
+                    hidden_states,
+                    attention_mask,
+                    causal_attention_mask,
+                    output_attentions=output_attentions,
+                )
+
+                hidden_states = layer_outputs[0]
+
+                if output_attentions:
+                    all_attentions = all_attentions + (layer_outputs[1],)
+
+            if output_hidden_states:
+                encoder_states = encoder_states + (hidden_states,)
+
+            return hidden_states
+
+        self.transformer.text_model.encoder.forward = encoder_forward.__get__(self.transformer.text_model.encoder)
+
+
+        def text_encoder_forward(
+            self,
+            input_ids = None,
+            attention_mask = None,
+            position_ids = None,
+            output_attentions = None,
+            output_hidden_states = None,
+            return_dict = None,
+            embedding_manager = None,
+            prospect_words = None
+        ):
+            output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+            output_hidden_states = (
+                output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+            )
+            return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+            if input_ids is None:
+                raise ValueError("You have to specify either input_ids")
+
+            # import pdb; pdb.set_trace()
+            input_shape = input_ids.size()
+            input_ids = input_ids.view(-1, input_shape[-1])
+
+            hidden_states = self.embeddings(input_ids=input_ids, position_ids=position_ids, 
+                                            embedding_manager=embedding_manager, prospect_words=prospect_words)
+
+            bsz, seq_len = input_shape
+            last_hidden_states = []
+
+            for i in range(len(hidden_states)):
+                hidden_state = hidden_states[i]
+            # CLIP's text model uses causal mask, prepare it here.
+            # https://github.com/openai/CLIP/blob/cfcffb90e69f37bf2ff1e988237a0fbe41f33c04/clip/model.py#L324
+                causal_attention_mask = _build_causal_attention_mask(bsz, seq_len, hidden_state.dtype).to(
+                    hidden_state.device
+                )
+
+                # expand attention_mask
+                if attention_mask is not None:
+                    # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+                    attention_mask = _expand_mask(attention_mask, hidden_state.dtype)
+
+                last_hidden_state = self.encoder(
+                    inputs_embeds=hidden_state,
+                    attention_mask=attention_mask,
+                    causal_attention_mask=causal_attention_mask,
+                    output_attentions=output_attentions,
+                    output_hidden_states=output_hidden_states,
+                    return_dict=return_dict,
+                )
+
+                last_hidden_state = self.final_layer_norm(last_hidden_state)
+                last_hidden_states.append(last_hidden_state)
+            
+
+            return last_hidden_states
+
+        self.transformer.text_model.forward = text_encoder_forward.__get__(self.transformer.text_model)
+
+        def transformer_forward(
+            self,
+            input_ids = None,
+            attention_mask = None,
+            position_ids = None,
+            output_attentions = None,
+            output_hidden_states = None,
+            return_dict = None,
+            embedding_manager = None,
+            prospect_words = None,
+        ):
+            return self.text_model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+                embedding_manager = embedding_manager,
+                prospect_words = prospect_words,
+            )
+
+        self.transformer.forward = transformer_forward.__get__(self.transformer)
+
+
+    def freeze(self):
+        self.transformer = self.transformer.eval()
+        for param in self.parameters():
+            param.requires_grad = False
+
+    def forward(self, text, prospect_words=None, **kwargs):
+        if prospect_words is not None:
+            print('Find Words:',prospect_words)
+        batch_encoding = self.tokenizer(text, truncation=True, max_length=self.max_length, return_length=True,
+                                        return_overflowing_tokens=False, padding="max_length", return_tensors="pt")
+        tokens = batch_encoding["input_ids"].to(self.device)        
+        # import pdb; pdb.set_trace()
+        z = self.transformer(input_ids=tokens, prospect_words = prospect_words, **kwargs)
+
+        return z
+
+    def encode(self, text, prospect_words=None, **kwargs):
+        return self(text, prospect_words=prospect_words, **kwargs)
+
+
 class FrozenCLIPEmbedder(AbstractEncoder):
     """Uses the CLIP transformer encoder for text (from Hugging Face)"""
     def __init__(self, version="openai/clip-vit-large-patch14", device="cuda", max_length=77):
