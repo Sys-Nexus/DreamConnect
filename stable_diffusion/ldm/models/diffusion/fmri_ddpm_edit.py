93a3a996f70b6bed73a298019c73bff568b0fd17
sunyasheng <1044705955@qq.com>
2024-03-03 02:18:33 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
+++ stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
@@ -646,10 +646,25 @@ class LatentDiffusion(DDPM):
             raise NotImplementedError(f"encoder_posterior of type '{type(encoder_posterior)}' not yet implemented")
         return self.scale_factor * z
 
-    def get_learned_conditioning(self, c):
+    # def get_learned_conditioning(self, c):
+    #     if self.cond_stage_forward is None:
+    #         if hasattr(self.cond_stage_model, 'encode') and callable(self.cond_stage_model.encode):
+    #             c = self.cond_stage_model.encode(c)
+    #             if isinstance(c, DiagonalGaussianDistribution):
+    #                 c = c.mode()
+    #         else:
+    #             c = self.cond_stage_model(c)
+    #     else:
+    #         assert hasattr(self.cond_stage_model, self.cond_stage_forward)
+    #         c = getattr(self.cond_stage_model, self.cond_stage_forward)(c)
+    #     return c
+
+    def get_learned_conditioning(self, c, prospect_words=None):
+        # import pdb; pdb.set_trace()
         if self.cond_stage_forward is None:
             if hasattr(self.cond_stage_model, 'encode') and callable(self.cond_stage_model.encode):
-                c = self.cond_stage_model.encode(c)
+                # c = self.cond_stage_model.encode(c)
+                c = self.cond_stage_model.encode(c,  prospect_words=prospect_words, embedding_manager=self.embedding_manager)
                 if isinstance(c, DiagonalGaussianDistribution):
                     c = c.mode()
             else:
@@ -658,7 +673,7 @@ class LatentDiffusion(DDPM):
             assert hasattr(self.cond_stage_model, self.cond_stage_forward)
             c = getattr(self.cond_stage_model, self.cond_stage_forward)(c)
         return c
-
+        
     def get_learned_conditioning_fmri(self, cc):
         if self.cond_stage_forward_fmri is None:
             if hasattr(self.cond_stage_model_fmri, 'encode') and callable(self.cond_stage_model_fmri.encode):
