b49addc5d0acf7db75db5b96bb78dcc07af619fc
sunyasheng <1044705955@qq.com>
2024-03-03 18:19:14 +0900

    up

--- third_party/versatile_diffusion/lib/model_zoo/ddim_vd_dual.py
+++ third_party/versatile_diffusion/lib/model_zoo/ddim_vd_dual.py
@@ -721,7 +721,7 @@ class DDIMSampler_Dual(DDIMSampler):
             
             if i == coarse_spatial_steps+1: 
                 x_dec_edit = self.stochastic_encode(x0=x_dec_edit_info[-1]*0.18215,
-                                                    t=torch.ones_like(edit_ts) * (index - coarse_spatial_steps),
+                                                    t=torch.ones_like(edit_ts) * (index + coarse_spatial_steps),
                                                     use_original_steps=use_original_steps)
                 # import pdb; pdb.set_trace()
             # import pdb; pdb.set_trace();
