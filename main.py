fc39177c87f13dca5e3b3417f921edab2f6be0ec
sunyasheng <1044705955@qq.com>
2024-03-05 16:47:44 +0900

    up

--- main.py
+++ main.py
@@ -400,7 +400,7 @@ def test_one_epoch(config, model, model_ema, data_loader, val_data_loader, optim
                 else:
                     model.log_images(batch, epoch, idx, val_idx, model_wrap, model_wrap_cfg, 
                                  save_dir, 'val', cfg_text=cfg_text, cfg_fmri=2.5,
-                                 prospect_words=prospect_words, is_inst_edit=is_inst_edit)
+                                 prospect_words=None, is_inst_edit=is_inst_edit)
     model.train()
 
 def train_one_epoch(config, model, model_ema, data_loader, val_data_loader, optimizer, epoch, 
@@ -787,7 +787,7 @@ if __name__ == "__main__":
         prospect_words = ['Change the picture to * style.'] #* 10
         test_one_epoch(config, model, model_ema, data_loader_train, data_loader_val, 
                         optimizer, epoch, lr_scheduler, scaler, model_wrap, model_wrap_cfg, visdir, 
-                        cfg_text=opt.cfg_text, prospect_words=prospect_words, is_inst_edit=args.is_inst_edit)
+                        cfg_text=opt.cfg_text, prospect_words=prospect_words, is_inst_edit=opt.is_inst_edit)
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
