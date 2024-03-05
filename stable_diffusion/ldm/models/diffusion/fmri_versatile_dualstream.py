cd551361911c94005bd9994298603a2ace61eaac
sunyasheng <1044705955@qq.com>
2024-03-05 21:42:33 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -535,23 +535,23 @@ class DualLDM(LatentDiffusion):
             c_w_uncond["c_crossattn_1"]["image_emb"] = [torch.cat([uncond_c0, c0, c0], 0)]
             c_w_uncond["c_crossattn_1"]["text_emb"] = [torch.cat([uncond_c1, c1, c1], 0)]
             c_w_uncond["c_crossattn"] = [torch.cat([null_prompt_emb, prompt_emb, prompt_emb], 0)]
-            if layout_in is not None:
-                c_w_uncond["c_crossattn"] = [torch.cat([null_prompt_emb, null_prompt_emb, null_prompt_emb], 0)]
+            if layout_in is None:
                 c_concat = F.interpolate(xc["c_concat"], (512, 512))
                 c_concat = self.encode_first_stage(c_concat).mode().detach()
                 c_w_uncond["c_concat"] = [torch.cat([c_concat, torch.zeros_like(c_concat), c_concat], 0)]
             else:
+                c_w_uncond["c_crossattn"] = [torch.cat([null_prompt_emb, null_prompt_emb, null_prompt_emb], 0)]
                 c_w_uncond["c_concat"] = [torch.cat([layout_concat, torch.zeros_like(layout_concat), layout_concat], 0)]
         else:
             c_w_uncond["c_crossattn_1"]["image_emb"] = [torch.cat([uncond_c0, c0], 0)]
             c_w_uncond["c_crossattn_1"]["text_emb"] = [torch.cat([uncond_c1, c1], 0)]
             c_w_uncond["c_crossattn"] = [torch.cat([null_prompt_emb, prompt_emb], 0)]
-            if layout_in is not None:
-                c_w_uncond["c_crossattn"] = [torch.cat([null_prompt_emb, prompt_emb], 0)]
+            if layout_in is None:
                 c_concat = F.interpolate(xc["c_concat"], (512, 512))
                 c_concat = self.encode_first_stage(c_concat).mode().detach()
                 c_w_uncond["c_concat"] = [torch.cat([c_concat, torch.zeros_like(c_concat), c_concat], 0)]
             else:
+                c_w_uncond["c_crossattn"] = [torch.cat([null_prompt_emb, null_prompt_emb], 0)]
                 c_w_uncond["c_concat"] = [torch.cat([layout_concat, torch.zeros_like(layout_concat), layout_concat], 0)]
 
         # z_enc = self.sampler.stochastic_encode(init_latent, torch.tensor([self.t_enc]).to(z_gt.device))
