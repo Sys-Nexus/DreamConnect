8ddc4e8c7d1ed0b02a72d5acdb2730cb6a94ff32
sunyasheng <1044705955@qq.com>
2024-03-06 17:43:11 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -510,10 +510,11 @@ class DualLDM(LatentDiffusion):
 
         c_w_uncond = copy.deepcopy(c)
 
-        import pdb; pdb.set_trace();
+        # import pdb; pdb.set_trace();
         c0 = torch.cat(c["c_crossattn_1"]["image_emb"], 1)
         if is_inst_gen is True:
             c1 = [self.get_learned_conditioning(['*'],prospect_words=['*']*10)[0].detach()]
+            c1 = torch.cat(c1, 1)
         else:
             # cond["c_crossattn"] = [self.get_learned_conditioning(['*'], prospect_words=prospect_words)[0].detach()]
             c1 = torch.cat(c["c_crossattn_1"]["text_emb"], 1)            
