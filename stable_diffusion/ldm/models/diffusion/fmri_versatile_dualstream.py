60a1b0fb8f678280b8071a7296de9176ccb14755
sunyasheng <1044705955@qq.com>
2024-03-06 23:23:35 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -518,7 +518,8 @@ class DualLDM(LatentDiffusion):
         if is_inst_gen is True:
             self.mixing = 1.0 # all text
             # self.mixing = 0.0 # all image
-            c1 = [self.get_learned_conditioning(['*'],prospect_words=['*']*10)[0].detach()]
+            # c1 = [self.get_learned_conditioning(['*'],prospect_words=['*']*10)[0].detach()]
+            c1 = [self.get_learned_conditioning(['A bird'])]
             c1 = torch.cat(c1, 1)
             z_enc_gen = torch.randn_like(z_enc_gen)
             # c1 = torch.cat(c["c_crossattn_1"]["text_emb"], 1)
