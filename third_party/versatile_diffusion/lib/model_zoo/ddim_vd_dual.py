b20e2836675ebd649fb611257999be9ab06fbd2f
sunyasheng <1044705955@qq.com>
2024-03-07 20:17:14 +0900

    up

--- third_party/versatile_diffusion/lib/model_zoo/ddim_vd_dual.py
+++ third_party/versatile_diffusion/lib/model_zoo/ddim_vd_dual.py
@@ -504,9 +504,9 @@ class DDIMSampler_Dual(DDIMSampler):
         t_gen_in = torch.cat([t_gen] * 3)
         t_edit_in = torch.cat([t_edit] * 3)
         
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         if len(cond_dict['c_crossattn_1']['text_emb']) > 1:
-            t_select = int(t.item()*self.prospect_stages/1000)
+            t_select = int(t_gen.item()*self.prospect_stages/1000)
             select_cond_dict = copy.deepcopy(cond_dict)
             select_cond_dict['c_crossattn_1']['text_emb'] = [cond_dict['c_crossattn_1']['text_emb'][t_select]]
             if t_select < 7: mixed_ratio = 0.6
