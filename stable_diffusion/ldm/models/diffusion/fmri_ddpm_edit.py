f4f575abfb1bcabe2a05b1a7fa4ef0ff7e61d9bd
sunyasheng <1044705955@qq.com>
2024-03-11 07:29:01 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
+++ stable_diffusion/ldm/models/diffusion/fmri_ddpm_edit.py
@@ -1137,7 +1137,6 @@ class LatentDiffusion(DDPM):
 
     ### TODO: what we should give to noise_edit
     def p_losses(self, x_start_gen, x_start_edit, cond, t, noise=None, noise_edit=None):
-        import pdb; pdb.set_trace();
         noise_gen = default(noise, lambda: torch.randn_like(x_start_gen))
         x_noisy_gen = self.q_sample(x_start=x_start_gen, t=t, noise=noise_gen)
 
--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -291,7 +291,7 @@ class DualLDM(LatentDiffusion):
 
     ### TODO: what we should give to noise_edit
     def p_losses(self, x_start_gen, x_start_edit, cond, t, noise=None, noise_edit=None, t_edit=None, is_return_x0=True):
-        # import pdb; pdb.set_trace();
+        import pdb; pdb.set_trace();
         noise_gen = default(noise, lambda: torch.randn_like(x_start_gen))
         x_noisy_gen = self.q_sample(x_start=x_start_gen, t=t, noise=noise_gen)
 
