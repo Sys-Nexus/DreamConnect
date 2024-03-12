d64ce6c377d0aac688de259128bd6a8b42fbae21
sunyasheng <1044705955@qq.com>
2024-03-12 10:40:38 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -61,7 +61,7 @@ class ControlledUnetModel(UNetModel):
         transformer_depth = self.transformer_depth
         force_type_convert = self.force_type_convert
         self.train_feat_adaptor = train_feat_adaptor
-
+        import pdb; pdb.set_trace()
         if train_feat_adaptor is True:
             ds = 8
             layers = []
