7eb19e99ed60b9fb59e667a6e148080bca3d082d
sunyasheng <1044705955@qq.com>
2024-03-12 19:32:38 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -82,7 +82,7 @@ class ControlledUnetModel(UNetModel):
                         if legacy:
                             #num_heads = 1
                             dim_head = ch // num_heads if use_spatial_transformer else num_head_channels
-                        if cnt in use_adaptor_layers:
+                        if cnt in self.use_adaptor_layers:
                             self.adaptor_blocks.append(SpatialTransformer(
                                     ch, num_heads, dim_head, default_eps=default_eps, force_type_convert=force_type_convert, depth=transformer_depth, context_dim=context_dim
                                 ))
