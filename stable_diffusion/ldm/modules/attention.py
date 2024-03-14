cdcb5a02de5be571e62e03e158f0b4ba578a98f6
sunyasheng <1044705955@qq.com>
2024-03-14 21:28:59 +0900

    up

--- stable_diffusion/ldm/modules/attention.py
+++ stable_diffusion/ldm/modules/attention.py
@@ -445,6 +445,58 @@ class SpatialTransformer(nn.Module):
         return x + x_in
 
 
+# class SpatialTransformer(nn.Module):
+#     """
+#     Transformer block for image-like data.
+#     First, project the input (aka embedding)
+#     and reshape to b, t, d.
+#     Then apply standard transformer action.
+#     Finally, reshape to image
+#     NEW: use_linear for more efficiency instead of the 1x1 convs
+#     """
+#     def __init__(self, in_channels, n_heads, d_head, default_eps, force_type_convert,
+#                  depth=1, dropout=0., context_dim=None,
+#                  disable_self_attn=False, use_linear=False,
+#                  use_checkpoint=True, is_simple_basic=False):
+#         super().__init__()
+#         if exists(context_dim) and not isinstance(context_dim, list):
+#             context_dim = [context_dim]
+#         self.in_channels = in_channels
+#         inner_dim = n_heads * d_head
+#         self.force_type_convert = force_type_convert
+#         self.norm = Normalize(in_channels, default_eps)
+#         if not use_linear:
+#             self.proj_in = nn.Conv2d(in_channels,
+#                                      inner_dim,
+#                                      kernel_size=1,
+#                                      stride=1,
+#                                      padding=0)
+#         else:
+#             self.proj_in = nn.Linear(in_channels, inner_dim)
+
+#         if is_simple_basic is True:
+#             self.transformer_blocks = nn.ModuleList(
+#                 [SimpleBasicTransformerBlock(inner_dim, n_heads, d_head, dropout=dropout, context_dim=context_dim[d],
+#                                     disable_self_attn=disable_self_attn, checkpoint=use_checkpoint)
+#                     for d in range(depth)]
+#             )
+#         else:
+#             self.transformer_blocks = nn.ModuleList(
+#                 [BasicTransformerBlock(inner_dim, n_heads, d_head, dropout=dropout, context_dim=context_dim[d],
+#                                     disable_self_attn=disable_self_attn, checkpoint=use_checkpoint)
+#                     for d in range(depth)]
+#             )
+#         if not use_linear:
+#             self.proj_out = zero_module(nn.Conv2d(inner_dim,
+#                                                   in_channels,
+#                                                   kernel_size=1,
+#                                                   stride=1,
+#                                                   padding=0))
+#         else:
+#             self.proj_out = zero_module(nn.Linear(in_channels, inner_dim))
+#         self.use_linear = use_linear
+#         self.checkpoint = checkpoint
+    
     # def forward(self, x, context=None):
     #     # note: if no context is given, cross-attention defaults to self-attention
     #     if not isinstance(context, list):
--- stable_diffusion/ldm/modules/diffusionmodules/util.py
+++ stable_diffusion/ldm/modules/diffusionmodules/util.py
@@ -148,8 +148,9 @@ class CheckpointFunction(torch.autograd.Function):
     @staticmethod
     @torch.cuda.amp.custom_bwd # add this
     def backward(ctx, *output_grads):
-        import pdb; pdb.set_trace()
-        ctx.input_tensors = [x.detach().requires_grad_(True) for x in ctx.input_tensors]
+        # import pdb; pdb.set_trace()
+        # ctx.input_tensors = [x.detach().requires_grad_(True) for x in ctx.input_tensors]
+        ctx.input_tensors = [x.detach().requires_grad_(True) for x in ctx.input_tensors if x is not None]
         with torch.enable_grad():
             # Fixes a bug where the first op in run_function modifies the
             # Tensor storage in place, which is not allowed for detach()'d
