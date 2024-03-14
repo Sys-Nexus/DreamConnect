247a6169ed178cbb6a901dc44d268d78218e16dc
sunyasheng <1044705955@qq.com>
2024-03-14 09:59:32 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -275,6 +275,8 @@ class DualLDM(LatentDiffusion):
         if ctx_adaptor_ckpt_path is not None and os.path.exists(ctx_adaptor_ckpt_path):
             pretrained_state_dict = torch.load(ctx_adaptor_ckpt_path, map_location="cpu")
             import pdb; pdb.set_trace()
+            ctx_pretrained_state_dict = {k.replace('diffusion.adaptor_blocks.',''): v for k,v in pretrained_state_dict['module'].items() if 'diffsion_model.adaptor_blocks.' in k}
+            missing, unexpected = self.model.diffusion_model.adaptor_blocks.load_state_dict(ctx_pretrained_state_dict, strict=False)
 
         self.prospect_stages = 10
         self.sampler = DDIMSampler_Dual(self)
