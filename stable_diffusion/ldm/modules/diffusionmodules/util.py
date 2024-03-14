516d93042af2791da7ffa77b707265af887050ab
sunyasheng <1044705955@qq.com>
2024-03-14 22:00:43 +0900

    up

--- stable_diffusion/ldm/modules/diffusionmodules/util.py
+++ stable_diffusion/ldm/modules/diffusionmodules/util.py
@@ -149,8 +149,7 @@ class CheckpointFunction(torch.autograd.Function):
     @torch.cuda.amp.custom_bwd # add this
     def backward(ctx, *output_grads):
         # import pdb; pdb.set_trace()
-        # ctx.input_tensors = [x.detach().requires_grad_(True) for x in ctx.input_tensors]
-        ctx.input_tensors = [x.detach().requires_grad_(True) for x in ctx.input_tensors if x is not None else None]
+        ctx.input_tensors = [x.detach().requires_grad_(True) for x in ctx.input_tensors]
         # yyyyy = []
         # for x in ctx.input_tensors:
         #     if x is not None:
