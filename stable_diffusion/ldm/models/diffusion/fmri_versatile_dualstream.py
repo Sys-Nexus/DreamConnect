d85899e801adf16c54ef3bfd411a44da056eebae
sunyasheng <1044705955@qq.com>
2024-03-15 20:50:44 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -618,14 +618,15 @@ class DualLDM(LatentDiffusion):
         fmri_cap = self.vd_clip.clip_encode_text(cap)
 
         # import pdb; pdb.set_trace();
-        if pred_image_emb is not None:
-            fmri_x, fmri_cap = pred_image_emb.half(), pred_text_emb.half()
 
         cond["c_crossattn_1"] = {}
 
         if self.only_align_loss is True:
             cond["c_crossattn_1"]["gt_image_emb"] = [fmri_x.detach().requires_grad_(True)]
             cond["c_crossattn_1"]["gt_text_emb"] = [fmri_cap.detach().requires_grad_(True)]
+
+        if pred_image_emb is not None:
+            fmri_x, fmri_cap = pred_image_emb.half(), pred_text_emb.half()
 
         if force_c_encode is False:
             cond["c_crossattn_1"]["image_emb"] = [torch.where(fmri_prompt_mask.bool(), fmri_null_x, fmri_x)]
