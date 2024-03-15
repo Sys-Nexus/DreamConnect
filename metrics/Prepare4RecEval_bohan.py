4c29bc8eb280c349438b012fc896e8fc7d2ba501
sunyasheng <1044705955@qq.com>
2024-03-15 12:43:49 +0900

    up

--- metrics/Prepare4RecEval_bohan.py
+++ metrics/Prepare4RecEval_bohan.py
@@ -16,8 +16,8 @@ transform = transforms.Compose([
 
 def main():
     # image_directory = '/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/logs/versatile_dualstream_instruct_pre_post_w_x0_prospect_versatile_dualstream_instruct_pre_post_w_x0_prospect_2024-03-08/visualize/images/val'
-    image_directory = '/data/yashengsun/Proj/MMEditresults_zip/code/diffuer/fmriinstructdiffusion-fusion_explore/recon_path2-981'
-    # image_directory = '/data/yashengsun/Proj/MMEditresults_zip/code/diffuer/fmriinstructdiffusion-fusion_explore/all_images_path2-981'
+    # image_directory = '/data/yashengsun/Proj/MMEditresults_zip/code/diffuer/fmriinstructdiffusion-fusion_explore/recon_path2-981'
+    image_directory = '/data/yashengsun/Proj/MMEditresults_zip/code/diffuer/fmriinstructdiffusion-fusion_explore/all_images_path2-981'
     # List all image files in the directory
     # image_files = [f for f in os.listdir(image_directory) if f.endswith(('.jpg', '.jpeg', '.png'))]
     image_files = glob.glob(os.path.join(image_directory, '*.png'))
@@ -39,8 +39,8 @@ def main():
     images = torch.stack(images)
     # import pdb; pdb.set_trace()
     # Save the tensor of images as a PyTorch checkpoint
+    torch.save(images, 'bohan_gt_images.pt')
     # torch.save(images, 'bohan_pred_images.pt')
-    torch.save(images, 'bohan_pred_images.pt')
 
 
 if __name__ == '__main__':
