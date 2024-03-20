1d821408db9bfab6423f817293802b4116f6ec0d
sunyasheng <1044705955@qq.com>
2024-03-20 23:34:09 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -67,6 +67,7 @@ class ZeroConvControlledUnetModel(UNetModel):
             ds = 8
             layers = []
             out_ch = 1280
+            cnt = 0
             self.adaptor_blocks = nn.ModuleList([])
             for level, mult in list(enumerate(channel_mult))[::-1]:
                 for i in range(num_res_blocks + 1):
@@ -90,7 +91,8 @@ class ZeroConvControlledUnetModel(UNetModel):
 
                     self.adaptor_blocks.append(
                         self.make_zero_conv(channels=ch, out_channels=ch))
-
+                    cnt += 1
+                    if cnt >=9: break
         scales = torch.logspace(0, -1, len(self.adaptor_blocks))  # 0.1 to 1.0
         scales = scales * conditioning_scale
         self.scales = scales
@@ -131,7 +133,7 @@ class ZeroConvControlledUnetModel(UNetModel):
 
                 h = module(h, emb, context)
 
-                if injected_contexts is not None:
+                if injected_contexts is not None and i < len(self.adaptor_blocks):
                     inject_context = injected_contexts[cnt]
                     inject_context = inject_context.detach().requires_grad_(True)
                     res_h = self.adaptor_blocks[i](inject_context, context)
