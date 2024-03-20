82d2b80d076f646e93925cd782984e04e3d9f8df
sunyasheng <1044705955@qq.com>
2024-03-20 23:26:28 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -82,13 +82,14 @@ class ZeroConvControlledUnetModel(UNetModel):
                         if legacy:
                             #num_heads = 1
                             dim_head = ch // num_heads if use_spatial_transformer else num_head_channels
-                        self.adaptor_blocks.append(
-                            self.make_zero_conv(channels=ch, out_channels=ch))
+                        # self.adaptor_blocks.append(
+                        #     self.make_zero_conv(channels=ch, out_channels=ch))
                     if level and i == num_res_blocks:
                         out_ch = ch
                         ds //= 2
-                        self.adaptor_blocks.append(
-                            self.make_zero_conv(channels=ch, out_channels=ch))
+
+                    self.adaptor_blocks.append(
+                        self.make_zero_conv(channels=ch, out_channels=ch))
 
         scales = torch.logspace(0, -1, len(self.adaptor_blocks))  # 0.1 to 1.0
         scales = scales * conditioning_scale
