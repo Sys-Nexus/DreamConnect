9304b48d34061b8fa847c35f4b07fd19e60b3855
sunyasheng <1044705955@qq.com>
2024-03-20 22:02:08 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -85,7 +85,7 @@ class ZeroConvControlledUnetModel(UNetModel):
                         out_ch = ch
                         ds //= 2
                         self.adaptor_blocks.append(
-                            self.make_zero_conv(channels=out_ch, out_channels=out_ch))
+                            self.make_zero_conv(channels=out_ch//2, out_channels=out_ch//2))
         
         scales = torch.logspace(0, -1, len(self.adaptor_blocks))  # 0.1 to 1.0
         scales = scales * conditioning_scale
