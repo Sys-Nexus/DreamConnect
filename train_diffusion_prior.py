4941e9a9d9108661ce28da9645058ceb4dee1c69
sunyasheng <1044705955@qq.com>
2024-03-24 10:07:05 +0900

    up

--- train_diffusion_prior.py
+++ train_diffusion_prior.py
@@ -91,7 +91,7 @@ def trainer(args, train_dl, val_dl, diffusion_prior, vd_clip, optimizer, distrib
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--max_lr', type=float, default=0.001)
-    parser.add_argument('--num_epochs', type=int, default=100)
+    parser.add_argument('--num_epoch', type=int, default=100)
     parser.add_argument('--local_rank', type=int, default=0)
     parser.add_argument('--clip_size', type=int, default=768)
     parser.add_argument('--jobname', type=str, default='latent_diffusion_image')
