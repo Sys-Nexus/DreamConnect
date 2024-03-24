4f0180e5498553ad9ee6751a56bd97b35672280a
sunyasheng <1044705955@qq.com>
2024-03-24 13:57:04 +0900

    up

--- train_diffusion_prior.py
+++ train_diffusion_prior.py
@@ -100,6 +100,7 @@ def trainer(args, train_dl, val_dl, diffusion_prior, vd_clip, optimizer, distrib
             val_loss_nce_sum = 0.
             val_loss_prior_sum = 0.
 
+        import pdb; pdb.set_trace();
         for train_i, (_, _, _, _, _, _, _, _, file_name, text_descr) in tqdm(enumerate(train_dl)):
             train_iter = train_i + len(train_dl)*epoch
             # torch.cuda.synchronize()
