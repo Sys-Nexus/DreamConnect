e50b2d3521c13eac5e1d71b331f4638dcb8215b1
sunyasheng <1044705955@qq.com>
2024-03-24 09:12:13 +0900

    up

--- train_diffusion_prior.py
+++ train_diffusion_prior.py
@@ -2,14 +2,15 @@ from types import SimpleNamespace
 import sys
 sys.path.append('third_party/versatile_diffusion')
 from lib.model_zoo.vd import VDCLIP
-
+import json
 
 def main():
     clip_cfg = {'symbol': 'clip',
                 'args': {},
                 'name': 'clip_frozen',
                 'type': 'clip_frozen'}
-    clip_cfg = SimpleNamespace(**clip_cfg)    
+    # clip_cfg = SimpleNamespace(**clip_cfg)    
+    clip_cfg = json.loads(json.dumps(clip_cfg))
     vd_clip = VDCLIP(clip_cfg)
 
 
