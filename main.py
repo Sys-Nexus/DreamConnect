14a806c5b6a97090d7646b484e50f68456b78178
sunyasheng <1044705955@qq.com>
2024-03-25 00:09:21 +0900

    up

--- main.py
+++ main.py
@@ -546,7 +546,7 @@ def train_one_epoch(config, model, model_ema, data_loader, val_data_loader, opti
         if (epoch * num_steps + idx) % 4000 == 0:
             with torch.no_grad():
                 # if random.uniform(0,1) > 0.5:
-                if False:
+                if True:
                     for val_idx, batch in enumerate(val_data_loader):
                         batch_size = batch['image'].shape[0]
                         if model_wrap is not None:
