dc5eee2158990d2b34e77aa4820964d8fc24c9f5
sunyasheng <1044705955@qq.com>
2024-03-24 17:18:28 +0900

    up

--- train_diffusion_prior.py
+++ train_diffusion_prior.py
@@ -179,8 +179,9 @@ def prepare_train_data(batch_dict, vd_clip, use_image_aug=True):
     if use_image_aug:
         image = img_augment(image)
     vd_clip.clip.fp16 = False
+    vd_clip.clip.cuda()
     clip_target = vd_clip.clip_encode_vision(image)
-    # import pdb; pdb.set_trace();
+    import pdb; pdb.set_trace();
     clip_target = clip_target.cuda()
     return voxel, clip_target
 
