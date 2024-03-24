701465cfd47314335860edfdb54bdf0c6e0766c1
sunyasheng <1044705955@qq.com>
2024-03-24 09:20:39 +0900

    up

--- train_diffusion_prior.py
+++ train_diffusion_prior.py
@@ -5,6 +5,7 @@ from lib.model_zoo.vd import VDCLIP
 
 import json
 import yaml
+from easydict import EasyDict
 
 def main():
     clip_cfg = {'symbol': 'clip',
@@ -13,7 +14,8 @@ def main():
                 'type': 'clip_frozen'}
     # clip_cfg = SimpleNamespace(**clip_cfg)    
     # clip_cfg = yaml.load(yaml.dump(clip_cfg),Loader=yaml.Loader)
-    clip_cfg = yaml.safe_load(yaml.dump(clip_cfg))
+    # clip_cfg = yaml.safe_load(yaml.dump(clip_cfg))
+    clip_cfg = EasyDict(clip_cfg)
     vd_clip = VDCLIP(clip_cfg)
 
 
