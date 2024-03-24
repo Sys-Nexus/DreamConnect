a2fdae255f005a77feed15dac05edc48ab169113
sunyasheng <1044705955@qq.com>
2024-03-24 09:05:44 +0900

    up

---
+++ train_diffusion_prior.py
@@ -0,0 +1,15 @@
+import sys
+from lib.model_zoo.vd import VDCLIP
+sys.path.append('third_party/versatile_diffusion')
+
+
+def main():
+    clip_cfg = {'symbol': 'clip',
+                'args': {},
+                'name': 'clip_frozen',
+                'type': 'clip_frozen'}
+    vd_clip = VDCLIP(clip_cfg)
+
+
+if __name__ == '__main__':
+    main()
