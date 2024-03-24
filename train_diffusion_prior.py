460034223df938a7f9b8de5ceb4a199d915daf36
sunyasheng <1044705955@qq.com>
2024-03-24 16:11:01 +0900

    up

--- train_diffusion_prior.py
+++ train_diffusion_prior.py
@@ -244,7 +244,7 @@ def main():
 
     clip_cfg = EasyDict(clip_cfg)
     vd_clip = VDCLIP(clip_cfg)
-
+ Toyota timing cover not me
     dataset_cfg_str = """
     train:
       target: third_party.StableDiffusionReconstruction.codes.utils.nsd_creater.NIPS23NSDDataset
