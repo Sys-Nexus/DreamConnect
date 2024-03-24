8a6a9357565c63fcceb2c0b4c0ff721bd7a9f223
sunyasheng <1044705955@qq.com>
2024-03-24 16:55:15 +0900

    up

--- train_diffusion_prior.py
+++ train_diffusion_prior.py
@@ -31,6 +31,16 @@ img_augment = AugmentationSequential(
     # data_keys=["input"],
 )
 
+def soft_clip_loss(preds, targs, temp=0.125):
+    clip_clip = (targs @ targs.T)/temp
+    brain_clip = (preds @ targs.T)/temp
+    
+    loss1 = -(brain_clip.log_softmax(-1) * clip_clip.softmax(-1)).sum(-1).mean()
+    loss2 = -(brain_clip.T.log_softmax(-1) * clip_clip.softmax(-1)).sum(-1).mean()
+    
+    loss = (loss1 + loss2)/2
+    return loss
+    
 @torch.no_grad()
 def prepare_train_data(batch_dict, vd_clip, use_image_aug=True):
     voxel = batch_dict['fmri'].cuda()
