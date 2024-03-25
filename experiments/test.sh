8e17b2a69b2bf6df1536c345c97ce349a619265d
sunyasheng <1044705955@qq.com>
2024-03-26 08:26:00 +0900

    up

--- experiments/test.sh
+++ experiments/test.sh
@@ -1,5 +1,5 @@
 
-jobname=${1:-'wo_prospect_ctx_adaptor_bs4'}
+jobname=${1:-'wo_prospect_ctx_adaptor_zeroconv_mattn_bs4'}
 filter_mode=${2:-'tune_sideconv'} #do not use no_filter, which will fail the memory
 
 gpus=${3:-'0,'}
