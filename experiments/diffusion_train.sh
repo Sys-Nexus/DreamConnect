84f9d6289cc474db99422ba4c06ae6dc1bfd0990
sunyasheng <1044705955@qq.com>
2024-03-25 20:28:30 +0900

    up

--- experiments/diffusion_train.sh
+++ experiments/diffusion_train.sh
@@ -1,1 +1,10 @@
-CUDA_VISIBLE_DEVICES=0 python train_diffusion_prior.py
+mode=${1-'image'}
+which_gpu=${1-1}
+
+if [[ 'mode' == 'image' ]]; then
+    CUDA_VISIBLE_DEVICES=${which_gpu} python train_diffusion_prior.py --mode 'image'
+fi
+
+if [[ 'mode' == 'text' ]]; then
+    CUDA_VISIBLE_DEVICES=${which_gpu} python train_diffusion_prior.py --mode 'text' --ckpt_path 'dummy'
+fi
