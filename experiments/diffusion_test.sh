e731b4983ad89293ab3872940d4101f184312b4d
sunyasheng <1044705955@qq.com>
2024-03-25 21:25:19 +0900

    up

---
+++ experiments/diffusion_test.sh
@@ -0,0 +1,13 @@
+mode=${1-'image'}
+which_gpu=${2-'1,'}
+
+if [[ $mode == 'image' ]]; then
+    CUDA_VISIBLE_DEVICES=${which_gpu} python train_diffusion_prior.py --mode 'image' \
+                --ckpt_path '/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_image/last.pth'
+fi
+
+if [[ $mode == 'text' ]]; then
+    CUDA_VISIBLE_DEVICES=${which_gpu} python train_diffusion_prior.py --mode 'text' \
+                --batch_size 64 \
+                --ckpt_path '/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_text/last.pth'
+fi
