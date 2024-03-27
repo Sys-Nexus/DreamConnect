060c97d491b946cff508fec10a5f68f69f49d60e
sunyasheng <1044705955@qq.com>
2024-03-27 16:10:29 +0900

    up

--- experiments/diffusion_train.sh
+++ experiments/diffusion_train.sh
@@ -8,7 +8,7 @@ fi
 
 if [[ $mode == 'text' ]]; then
     CUDA_VISIBLE_DEVICES=${which_gpu} python train_diffusion_prior.py --mode 'text' \
-                --batch_size 64 \
+                --batch_size 128 \
                 --ckpt_path '/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_text/last.pth' \
                 --max_epoch 960
 fi
