31d999c32fa32f2fe737d771216db560f0b14ab9
sunyasheng <1044705955@qq.com>
2024-04-01 20:39:20 +0900

    up

--- experiments/test_style_control.sh
+++ experiments/test_style_control.sh
@@ -1,6 +1,7 @@
 
 jobname=${1:-'prospect_test_conv_adaptor_mixing0_image'}
-# jobname=${1:-'wo_prospect_ctx_adaptor_zeroconv_mattn_bs4_mixing1_text'}
+# jobname=${1:-'prospect_test_conv_adaptor_mixing1_text'}
+# jobname=${1:-'prospect_test_conv_adaptor'}
 filter_mode=${2:-'tune_sideconv'} #do not use no_filter, which will fail the memory
 
 gpus=${3:-'0,'}
