9a4940667f1d55722ea4f2fd0a3cc5a201d367bf
sunyasheng <1044705955@qq.com>
2024-04-01 10:43:19 +0900

    up

--- train_diffusion_prior.py
+++ train_diffusion_prior.py
@@ -355,7 +355,7 @@ def trainer(args, train_dl, val_dl, diffusion_prior, vd_clip, optimizer, distrib
 
                     clip_voxels, clip_voxels_proj = diffusion_prior.module.voxel2clip(voxel) if distributed else diffusion_prior.voxel2clip(voxel)
                     clip_voxels, clip_voxels_proj = clip_voxels.float(), clip_voxels_proj.float()
-                    # import pdb; pdb.set_trace()
+                    import pdb; pdb.set_trace()
                     
                     if hidden:
                         clip_voxels = clip_voxels.view(len(voxel),-1,clip_size)
