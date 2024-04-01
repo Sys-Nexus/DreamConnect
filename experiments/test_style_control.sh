eb197b92c8b56f97884b86a1f57ac33759333dbf
sunyasheng <1044705955@qq.com>
2024-04-01 20:25:43 +0900

    up

--- experiments/test_style_control.sh
+++ experiments/test_style_control.sh
@@ -7,9 +7,6 @@ gpus=${3:-'0,'}
 master_port=${4:-'27699'}
 isTrain=${5:-0}
 cfg_text=${6:-7.5}
-# cfg_text_edit=${6:-20.0}
-cfg_text_edit=${6:-2.5}
-# cfg_text_edit=${6:-7.5}
 mode=${7:-'single'}
 vis=${8:-1}
 
@@ -28,35 +25,105 @@ export PATH=/usr/local/cuda-11.3/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-11.3/lib64:$LD_LIBRARY_PATH
 
 current_date=$(date +"%Y-%m-%d")
-exp_name=${jobname}"_"${current_date}
+
+prospect_ckpt_paths=(
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/starryNew2024-03-04T23-34-17_starryNew/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/window_grille2024-03-04T23-28-37_window_grille/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/circuit2024-03-04T23-23-03_circuit/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/blackstrokes2024-03-04T23-17-24_blackstrokes/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/udnie2024-03-04T23-11-50_udnie/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/cloud2024-03-04T23-06-12_cloud/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/stars2024-03-04T23-00-36_stars/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/leaves2024-03-04T22-54-59_leaves/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/yellow_square2024-03-04T22-49-23_yellow_square/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/occean2024-03-04T22-43-46_occean/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/rain_princess2024-03-04T22-38-07_rain_princess/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/wave2024-03-04T22-32-31_wave/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/202004282208292024-03-04T22-26-58_20200428220829/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/colorful_cubes2024-03-04T22-21-24_colorful_cubes/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/bricks2024-03-04T22-15-50_bricks/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/feathers2024-03-04T22-10-18_feathers/checkpoints/embeddings.pt
+  '/data/yashengsun/Proj/MMEdit/ProSpect/logs_backup0305/kanagawa2024-02-29T06-01-27_kanagawa/checkpoints/embeddings.pt'
+)
+
+style_names=(
+  'bricks'
+  'leaves'
+  'cloud'
+  'colorful_cubes'
+  'window_grille'
+  'yellow_square'
+  'wave'
+  'udnie'
+  'rain_princess'
+  'feathers'
+  'blackstrokes'
+  '20200428220829'
+  'stars'
+  'starryNew'
+  'occean'
+  'circuit'
+  'kanagawa'
+)
+
+cfg_text_edits=(
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+  3.5
+)
+
+
+layout_path='/data/yashengsun/Proj/MMEdit/ProSpect/images4prospect/layout_collections/baozi/baozi.jpg'
+resume_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/logs/wo_prospect_ctx_adaptor_bs4_wo_prospect_ctx_adaptor_bs4_2024-03-13/checkpoints/ckpt_epoch_5'
 
 # prospect_ckpt_path='/data/yashengsun/Proj/MMEdit/ProSpect/logs/apple2024-02-28T22-30-10_apple/checkpoints/embeddings.pt'
 # prospect_ckpt_path='/data/yashengsun/Proj/MMEdit/ProSpect/logs/panda2024-02-29T05-29-09_panda/checkpoints/embeddings.pt'
 # prospect_ckpt_path='/data/yashengsun/Proj/MMEdit/ProSpect/logs_backup0305/panda2024-02-29T05-29-09_panda/checkpoints/embeddings.pt'
-prospect_ckpt_path='/data/yashengsun/Proj/MMEdit/ProSpect/logs_backup0305/kanagawa2024-02-29T06-01-27_kanagawa/checkpoints/embeddings.pt'
-layout_path='/data/yashengsun/Proj/MMEdit/ProSpect/images4prospect/layout_collections/baozi/baozi.jpg'
+# prospect_ckpt_path='/data/yashengsun/Proj/MMEdit/ProSpect/logs_backup0305/kanagawa2024-02-29T06-01-27_kanagawa/checkpoints/embeddings.pt'
 # resume_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/checkpoints/bohan_recon/'
-resume_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/logs/wo_prospect_ctx_adaptor_bs4_wo_prospect_ctx_adaptor_bs4_2024-03-13/checkpoints/ckpt_epoch_5'
-
 # ${HOME}/.local/bin/wandb login 56d149bd571b8312fbca5e3802d7859909ea00c1
 
-CUDA_VISIBLE_DEVICES=${gpus} ${run_cmd} main.py --name ${exp_name} \
-    --base configs/test/${jobname}.yaml \
-    --train \
-    --gpus ${gpus} \
-    --resume '' \
-    --num_nodes ${num_nodes} \
-    --filter_mode ${filter_mode} \
-    --no-test True \
-    --vis ${vis} \
-    --isTrain ${isTrain} \
-    --cfg_text ${cfg_text} \
-    --prospect_ckpt_path ${prospect_ckpt_path} \
-    --cfg_text_edit ${cfg_text_edit} \
-    --is_inst_gen True \
-    --is_inst_edit True
-    # --layout_path ${layout_path} #\
-    
+length=${#style_names[@]}
+
+for ((i = 0; i < length; i++)); do
+  style_name=${style_names[i]}
+  prospect_ckpt_path=${prospect_ckpt_paths[i]}
+  cfg_text_edit=${cfg_text_edits[i]}
+
+  exp_name=${jobname}"_"${current_date}"_"${style_name}
+
+  CUDA_VISIBLE_DEVICES=${gpus} ${run_cmd} main.py --name ${exp_name} \
+      --base configs/test/${jobname}.yaml \
+      --train \
+      --gpus ${gpus} \
+      --resume '' \
+      --num_nodes ${num_nodes} \
+      --filter_mode ${filter_mode} \
+      --no-test True \
+      --vis ${vis} \
+      --isTrain ${isTrain} \
+      --cfg_text ${cfg_text} \
+      --prospect_ckpt_path ${prospect_ckpt_path} \
+      --cfg_text_edit ${cfg_text_edit} \
+      --is_inst_gen True \
+      --is_inst_edit True
+done
+
+
+# --layout_path ${layout_path} #\
 ## set both is_inst_gen and is_inst_edit to True for correct inference
 ## is_inst_gen: control the generation process of versatile diffusion
 ## is_inst_edit: control the editing process of instruction diffusion
--- main.py
+++ main.py
@@ -416,6 +416,9 @@ def test_one_epoch(config, model, model_ema, data_loader, val_data_loader, optim
                     model.log_images(batch, epoch, idx, val_idx, model_wrap, model_wrap_cfg, 
                                  save_dir, 'val', cfg_text=cfg_text, cfg_text_edit=cfg_text_edit, cfg_fmri=2.5,
                                  prospect_words=prospect_words, is_inst_edit=is_inst_edit, is_inst_gen=is_inst_gen)
+                    if val_idx > 100: break
+                    ## only run 100 iterations for quick selection of images
+                    
                 # elif layout_in is not None:
                 #     model.log_images(batch, epoch, idx, val_idx, model_wrap, model_wrap_cfg, 
                 #                  save_dir, 'val', cfg_text=cfg_text, cfg_text_edit=cfg_text_edit, cfg_fmri=2.5,
