6bb19a5afc24a8446eb0f178b50a05f26030b12d
sunyasheng <1044705955@qq.com>
2024-04-01 11:59:59 +0900

    up

--- train_diffusion_prior.py
+++ train_diffusion_prior.py
@@ -361,7 +361,7 @@ def trainer(args, train_dl, val_dl, diffusion_prior, vd_clip, optimizer, distrib
                     if hidden:
                         clip_voxels = clip_voxels.view(len(voxel),-1,clip_size)
                     
-                    print(clip_target.max(), clip_target.min(), clip_target.mean())
+                    # print(clip_target.max(), clip_target.min(), clip_target.mean())
                     # print(clip_voxels.max(), clip_voxels.min(), clip_voxels.mean())
                     if prior:
                         loss_prior, aligned_clip_voxels = diffusion_prior(text_embed=clip_voxels, image_embed=clip_target)
