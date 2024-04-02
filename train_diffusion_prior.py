bb2050a941e05ad949cd862f6ff47261d8ec7cd0
sunyasheng <1044705955@qq.com>
2024-04-02 14:58:15 +0900

    up

--- train_diffusion_prior.py
+++ train_diffusion_prior.py
@@ -655,7 +655,8 @@ def main():
 
     # use dalle interface to include prior model and clip text-to-emotion models
     timesteps = 100
-    image_embed_scale = 1.0 #if args.mode == 'text' else None
+    # image_embed_scale = 1.0 #if args.mode == 'text' else None
+    image_embed_scale=None
     diffusion_prior = InstructDiffusionPrior(
         net=prior_network,
         image_embed_dim=clip_size,
