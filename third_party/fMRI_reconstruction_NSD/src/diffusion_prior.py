a8438fd2c0686e790a4013eb472f74950cc7575e
sunyasheng <1044705955@qq.com>
2024-04-03 20:28:29 +0900

    up

--- third_party/fMRI_reconstruction_NSD/src/diffusion_prior.py
+++ third_party/fMRI_reconstruction_NSD/src/diffusion_prior.py
@@ -116,6 +116,7 @@ class BrainNetwork(nn.Module):
         x = x.reshape(len(x), -1)
         x = self.lin1(x)
         if self.use_projector:
+            import pdb; pdb.set_trace()
             proj_x = self.projector(x.reshape(len(x), -1, self.clip_size))
             return x, proj_x
         return x
