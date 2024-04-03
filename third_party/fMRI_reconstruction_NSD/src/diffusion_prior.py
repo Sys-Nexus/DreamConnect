1e5e3fe2c57ce88be41bff71fce91cc3c34b58dd
sunyasheng <1044705955@qq.com>
2024-04-03 20:23:02 +0900

    up

--- third_party/fMRI_reconstruction_NSD/src/diffusion_prior.py
+++ third_party/fMRI_reconstruction_NSD/src/diffusion_prior.py
@@ -85,13 +85,13 @@ class BrainNetwork(nn.Module):
         if use_projector:
             self.projector = nn.Sequential(
                 nn.LayerNorm(clip_size),
-                nn.GELU(),
+                nn.GELU(inplace=False),
                 nn.Linear(clip_size, 2048),
                 nn.LayerNorm(2048),
-                nn.GELU(),
+                nn.GELU(inplace=False),
                 nn.Linear(2048, 2048),
                 nn.LayerNorm(2048),
-                nn.GELU(),
+                nn.GELU(inplace=False),
                 nn.Linear(2048, clip_size)
             )
         
