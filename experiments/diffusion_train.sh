d1dd063e85efe06daef65013e68e2285be166bd4
sunyasheng <1044705955@qq.com>
2024-04-03 21:24:46 +0900

    up

--- experiments/diffusion_train.sh
+++ experiments/diffusion_train.sh
@@ -5,8 +5,8 @@ which_gpu=${2-'2,'}
 if [[ $mode == 'image' ]]; then
     # ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_image_backup0326/last.pth'
     # ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_image_from_scratch/last.pth'
-    # ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_image_from_scratch/ep_75.pth'
-    ckpt_path='dummy'
+    ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_image_from_scratch/ep_75.pth'
+    # ckpt_path='dummy'
     CUDA_VISIBLE_DEVICES=${which_gpu} python train_diffusion_prior.py --mode 'image' \
                 --max_epoch 240 \
                 --ckpt_path ${ckpt_path} \
@@ -17,8 +17,8 @@ fi
 if [[ $mode == 'text' ]]; then
     # ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_text_pct/ep_175.pth'
     # ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_text_pct_all/last.pth'
-    # ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_text_from_scratch/ep_150.pth'
-    ckpt_path='dummy'
+    ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_text_from_scratch/ep_150.pth'
+    # ckpt_path='dummy'
     CUDA_VISIBLE_DEVICES=${which_gpu} python train_diffusion_prior.py --mode 'text' \
                 --batch_size 128 \
                 --ckpt_path ${ckpt_path} \
