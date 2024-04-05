9c3fc3970d1a090033d6677a2bed26defaf6cbad
sunyasheng <1044705955@qq.com>
2024-04-05 12:27:13 +0900

    up

--- experiments/diffusion_test.sh
+++ experiments/diffusion_test.sh
@@ -2,7 +2,7 @@ mode=${1-'image'}
 which_gpu=${2-'1,'}
 
 if [[ $mode == 'image' ]]; then
-    # ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_image_from_scratch/ep_75.pth'
+    # ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_image_from_scratch/ep_75.pth' # eval
     ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_image_fp32/last.pth'
     CUDA_VISIBLE_DEVICES=${which_gpu} python train_diffusion_prior.py --mode 'image' --is_test True \
                 --batch_size 1 \
@@ -12,7 +12,8 @@ if [[ $mode == 'image' ]]; then
 fi
 
 if [[ $mode == 'text' ]]; then
-    ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_text_fp32/last.pth'
+    # ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_text_fp32/last.pth' # eval
+    ckpt_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/train_logs/latent_diffusion_text_fp32_resume/last.pth' # eval
     CUDA_VISIBLE_DEVICES=${which_gpu} python train_diffusion_prior.py --mode 'text' --is_test True \
                 --batch_size 1 \
                 --ckpt_path ${ckpt_path}
