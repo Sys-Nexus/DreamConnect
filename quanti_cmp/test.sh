1bd69eee87f30883558da03610d39546312cce72
sunyasheng <1044705955@qq.com>
2024-04-09 20:12:13 +0900

    up

--- quanti_cmp/test.sh
+++ quanti_cmp/test.sh
@@ -3,6 +3,6 @@ gpu_id=${2-'2,'}
 
 
 if [[ ${jobname} == 'magic_brush' ]]; then
-    CUDA_VISIBLE_DEVICES=${gpu_id} python instruct_cmp.py
+    CUDA_VISIBLE_DEVICES=${gpu_id} python quanti_cmp/instruct_cmp.py
 fi
 
