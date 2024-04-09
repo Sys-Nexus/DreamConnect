93eee9004f646cf4a2cd15f558bcc858f5d581dc
sunyasheng <1044705955@qq.com>
2024-04-09 21:06:25 +0900

    up

--- quanti_cmp/instruct_cmp.py
+++ quanti_cmp/instruct_cmp.py
@@ -97,6 +97,7 @@ def main():
     parser.add_argument('--method', type=str, default='magic_brush')
     parser.add_argument('--ckpt_path', type=str, default='')
     parser.add_argument('--recon_root', type=str, default='logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val/')
+    # parser.add_argument('--instruct_root', type=str, default='logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val/')
     args = parser.parse_args()
 
     dataset_cfg_str = """
@@ -130,11 +131,15 @@ def main():
     else:
         raise ValueError
 
+    args.instruct_root = args.recon_root.replace('images/val', 'images/val_{}'.format(method))
+    os.makedirs(args.instruct_root, exist_ok=True)
+
     model, model_wrap, model_wrap_cfg, sigmas, null_token = init_gen_model(ckpt_path=args.ckpt_path)
     image_path_templ = 'all_iter-999999_ep-999999_bidx-{:06d}-{:06d}.png'
     for val_i, batch_dict in tqdm(enumerate(val_dl)):
         # import pdb; pdb.set_trace()
-        image_path = os.path.join(args.recon_root, image_path_templ.format(val_i, batch_dict['s'].item()))
+        image_name = image_path_templ.format(val_i, batch_dict['s'].item())
+        image_path = os.path.join(args.recon_root, image_name)
         cond = {}
         instruct_text = batch_dict['fmri_edit']['c_crossattn']
         cond["c_crossattn"] = [model.get_learned_conditioning(instruct_text)]
@@ -157,8 +162,10 @@ def main():
         z = torch.randn_like(cond["c_concat"][0]) * sigmas[0]
         z = K.sampling.sample_euler_ancestral(model_wrap_cfg, z, sigmas, extra_args=extra_args)
         x = model.decode_first_stage(z)
+        input_image_all[:,3*512:4*512,:] = x[0,]
+        save_name = image_name.repalce(arg.recon_root, arg.instruct_root)
         import pdb; pdb.set_trace();
-
+        torchvision.utils.save_image(input_image_all, os.path.join(args.instruct_root, save_name))
 
 if __name__ == '__main__':
     main()
