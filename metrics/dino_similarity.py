0ef806a03d78f56366c3c415f31403a970396435
sunyasheng <1044705955@qq.com>
2024-04-12 19:40:07 +0900

    up

--- metrics/dino_similarity.py
+++ metrics/dino_similarity.py
@@ -23,8 +23,13 @@ class DINO_v2_Similarity(nn.Module):
         backbone_model.cuda()
         self.backbone_model = backbone_model
     
+        self.register_buffer("mean", torch.tensor((0.48145466, 0.4578275, 0.40821073)))
+        self.register_buffer("std", torch.tensor((0.26862954, 0.26130258, 0.27577711)))
+
     def encode_image(self, image):
         image = F.interpolate(image, (224,224))
+        image = image - rearrange(self.mean, "c -> 1 c 1 1")
+        image = image / rearrange(self.std, "c -> 1 c 1 1")
         image_features = self.backbone_model(image)
         image_features = image_features / image_features.norm(dim=1, keepdim=True)
         return image_features
