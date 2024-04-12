9d767e4da3019d5be98facbb6f45fd63ed64e62a
sunyasheng <1044705955@qq.com>
2024-04-12 17:11:27 +0900

    up

--- metrics/edit_eval.py
+++ metrics/edit_eval.py
@@ -4,8 +4,9 @@ import argparse
 import glob
 from PIL import Image
 import torchvision.transforms as transforms
+import torchvision
 from clip_similarity import ClipSimilarity
-
+from tqdm import tqdm
 
 
 def read_split_image(img_path, offset):
@@ -13,7 +14,7 @@ def read_split_image(img_path, offset):
     w = img.size[0]
     img = img.crop((0, offset*w, w, offset*w+w))
     img_ts = transforms.ToTensor()(img)
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     return img_ts
 
 
@@ -28,14 +29,15 @@ def main():
     sim_evaluator = ClipSimilarity().cuda()
     img_paths = glob.glob(os.path.join(args.root_dir, 'all_iter*.png'))
     all_id_sims = []
-    for img_path in img_paths:
+    for img_path in tqdm(img_paths):
         input_img = read_split_image(img_path, 3)
         edit_img = read_split_image(img_path, 4)
         input_feat = sim_evaluator.encode_image(input_img)
         edit_feat = sim_evaluator.encode_image(edit_img)
         id_sim = F.cosine_similarity(input_feat, edit_feat)
         all_id_sims.append(id_sim.item())
-    ave_id_sim = sum(all_id_sims) / len(all_id_sims)
+    ave_id_sim = sum(all_id_sims) * 1.0 / len(all_id_sims)
+    print('root_dir: ', args.root_dir)
     print('id_sim: ', ave_id_sim)
 
     # import pdb; pdb.set_trace()
