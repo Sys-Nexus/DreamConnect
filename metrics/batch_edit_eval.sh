08846c79cc14167404d807ec6cac3da3a24325e0
sunyasheng <1044705955@qq.com>
2024-04-12 18:04:17 +0900

    up

--- metrics/batch_edit_eval.sh
+++ metrics/batch_edit_eval.sh
@@ -1,4 +1,10 @@
-python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_pix2pix
-python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_dif
-python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_magic_brush
-python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
+# python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_pix2pix
+# python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_dif
+# python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_magic_brush
+# python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
+
+
+python  metrics/edit_eval.py --eval_method dino --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_pix2pix
+python  metrics/edit_eval.py --eval_method dino --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_dif
+python  metrics/edit_eval.py --eval_method dino --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_magic_brush
+python  metrics/edit_eval.py --eval_method dino --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
