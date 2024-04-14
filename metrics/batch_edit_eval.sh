7d93e4b6f63e98e06d3edcc7db8f68826ec1fc4e
sunyasheng <1044705955@qq.com>
2024-04-14 17:00:56 +0900

    up

--- metrics/batch_edit_eval.sh
+++ metrics/batch_edit_eval.sh
@@ -6,14 +6,21 @@ log_dirs=(
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_magic_brush
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
 )
+eval_modes=(
+    'id_sim'
+    'dino'
+    'inst_sim'
+)
 
-eval_mode='inst_sim'
+# eval_mode='inst_sim'
 # eval_mode='id_sim'
 # eval_method='dino'
 
 eval_method='clip'
-# root_dir=${log_dirs[0]}
-for root_dir in ${log_dirs[@]}
+for eval_mode in ${eval_modes[@]}
 do
-    python  metrics/edit_eval.py --eval_method ${eval_method} --eval_mode ${eval_mode} --root_dir ${root_dir}
+    for root_dir in ${log_dirs[@]}
+    do
+        python  metrics/edit_eval.py --eval_method ${eval_method} --eval_mode ${eval_mode} --root_dir ${root_dir}
+    done
 done
