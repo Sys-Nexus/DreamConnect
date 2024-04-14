4ade774f961e3abd54a08bc29c0a90a59217fe4a
sunyasheng <1044705955@qq.com>
2024-04-14 16:53:15 +0900

    up

--- metrics/batch_edit_eval.sh
+++ metrics/batch_edit_eval.sh
@@ -1,22 +1,11 @@
 log_dirs=(
-    logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val
-    logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_pix2pix
-    logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_dif
-    logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_magic_brush
-    logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
+    logs/test_conv_adaptor_test_conv_adaptor_2024-04-13/visualize/images/val 
+    # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val
+    # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_pix2pix
+    # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_dif
+    # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_magic_brush
+    # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
 )
-
-# python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_pix2pix
-# python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_dif
-# python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_magic_brush
-# python  metrics/edit_eval.py --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
-
-
-# python  metrics/edit_eval.py --eval_method dino --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_pix2pix
-# python  metrics/edit_eval.py --eval_method dino --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_dif
-# python  metrics/edit_eval.py --eval_method dino --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_magic_brush
-# python  metrics/edit_eval.py --eval_method dino --root_dir logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
-
 
 eval_mode='inst_sim'
 # eval_mode='id_sim'
