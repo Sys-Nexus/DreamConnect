ae8f3ecaab5e0cdd1ff02537bc6cfd827620b4f7
sunyasheng <1044705955@qq.com>
2024-04-14 17:10:21 +0900

    up

--- metrics/batch_edit_eval.sh
+++ metrics/batch_edit_eval.sh
@@ -6,22 +6,45 @@ log_dirs=(
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_magic_brush
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
 )
+
+
 eval_modes=(
     'id_sim'
-    'dino'
-    'inst_sim'
 )
-
-# eval_mode='inst_sim'
-# eval_mode='id_sim'
-# eval_method='dino'
-
-eval_method='clip'
+eval_methods=(
+    'dino'
+    'clip'
+)
 for eval_mode in ${eval_modes[@]}
 do
     echo 'eval_mode: '$eval_mode
-    for root_dir in ${log_dirs[@]}
+    for eval_method in ${eval_methods[@]}
     do
-        python  metrics/edit_eval.py --eval_method ${eval_method} --eval_mode ${eval_mode} --root_dir ${root_dir}
+        echo 'eval_method: '$eval_method
+        for root_dir in ${log_dirs[@]}
+        do
+            python  metrics/edit_eval.py --eval_method ${eval_method} --eval_mode ${eval_mode} --root_dir ${root_dir}
+        done
+    done
+done
+
+
+eval_modes=(
+    'inst_sim'
+)
+eval_methods=(
+    'clip'
+)
+
+for eval_mode in ${eval_modes[@]}
+do
+    echo 'eval_mode: '$eval_mode
+    for eval_method in ${eval_methods[@]}
+    do
+        echo 'eval_method: '$eval_method
+        for root_dir in ${log_dirs[@]}
+        do
+            python  metrics/edit_eval.py --eval_method ${eval_method} --eval_mode ${eval_mode} --root_dir ${root_dir}
+        done
     done
 done
