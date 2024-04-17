e85439bc2e90efa38a821f46048f91d37f2484c7
sunyasheng <1044705955@qq.com>
2024-04-18 01:52:37 +0900

    up

--- metrics/edit_eval.py
+++ metrics/edit_eval.py
@@ -76,7 +76,7 @@ def main():
         else:
             raise ValueError
         all_id_sims.append(id_sim.item())
-        if i>25:break
+        # if i>25:break
     ave_id_sim = sum(all_id_sims) * 1.0 / len(all_id_sims)
     print('eval_mode: ', args.eval_mode)
     print('root_dir: ', args.root_dir)
