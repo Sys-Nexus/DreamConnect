4b0e41f3a6b08853d0578b4ae24c5a138acd3e09
sunyasheng <1044705955@qq.com>
2024-04-18 02:16:22 +0900

    up

--- metrics/batch_edit_eval.sh
+++ metrics/batch_edit_eval.sh
@@ -1,8 +1,8 @@
 log_dirs=(
-    # logs/test_conv_adaptor_idback_test_conv_adaptor_idback_2024-04-17/visualize/images/val
+    logs/test_conv_adaptor_idback_test_conv_adaptor_idback_2024-04-17/visualize/images/val
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-13/visualize/images/val 
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val
-    logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_pix2pix
+    # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_pix2pix
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_dif
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_magic_brush
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
