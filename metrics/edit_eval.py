569ff3c76edb794b59bbe23074e341d5d3b16852
sunyasheng <1044705955@qq.com>
2024-04-18 02:19:29 +0900

    up

--- metrics/edit_eval.py
+++ metrics/edit_eval.py
@@ -48,6 +48,7 @@ def main():
         raise ValueError
 
     img_paths = sorted(glob.glob(os.path.join(args.root_dir, 'all_iter*.png')))
+    img_paths = img_paths[:6]
     all_id_sims = []
     for i,img_path in tqdm(enumerate(img_paths)):
         input_img = read_split_image(img_path, 3)
@@ -76,7 +77,7 @@ def main():
         else:
             raise ValueError
         all_id_sims.append(id_sim.item())
-        if i>5:break
+        # if i>5:break
     ave_id_sim = sum(all_id_sims) * 1.0 / len(all_id_sims)
     print('eval_mode: ', args.eval_mode)
     print('root_dir: ', args.root_dir)
