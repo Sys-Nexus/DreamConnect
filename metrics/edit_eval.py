5786fb1e4a2a9fc26ed6ae63ce29e2c2f8d0bab7
sunyasheng <1044705955@qq.com>
2024-04-18 02:19:54 +0900

    up

--- metrics/edit_eval.py
+++ metrics/edit_eval.py
@@ -49,6 +49,7 @@ def main():
 
     img_paths = sorted(glob.glob(os.path.join(args.root_dir, 'all_iter*.png')))
     img_paths = img_paths[:6]
+    print(img_paths)
     all_id_sims = []
     for i,img_path in tqdm(enumerate(img_paths)):
         input_img = read_split_image(img_path, 3)
