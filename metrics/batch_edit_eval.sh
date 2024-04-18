e8787a0b6d37713f61d4071befa39fff7df381e6
sunyasheng <1044705955@qq.com>
2024-04-18 15:20:23 +0900

    up

--- metrics/batch_edit_eval.sh
+++ metrics/batch_edit_eval.sh
@@ -16,18 +16,18 @@ eval_methods=(
     'dino'
     'clip'
 )
-for eval_mode in ${eval_modes[@]}
-do
-    echo 'eval_mode: '$eval_mode
-    for eval_method in ${eval_methods[@]}
-    do
-        echo 'eval_method: '$eval_method
-        for root_dir in ${log_dirs[@]}
-        do
-            python  metrics/edit_eval.py --eval_method ${eval_method} --eval_mode ${eval_mode} --root_dir ${root_dir}
-        done
-    done
-done
+# for eval_mode in ${eval_modes[@]}
+# do
+#     echo 'eval_mode: '$eval_mode
+#     for eval_method in ${eval_methods[@]}
+#     do
+#         echo 'eval_method: '$eval_method
+#         for root_dir in ${log_dirs[@]}
+#         do
+#             python  metrics/edit_eval.py --eval_method ${eval_method} --eval_mode ${eval_mode} --root_dir ${root_dir}
+#         done
+#     done
+# done
 
 
 eval_modes=(
