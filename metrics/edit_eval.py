a8450397b75629f5b0bba06d0a9213f4a59a5467
sunyasheng <1044705955@qq.com>
2024-04-18 20:37:49 +0900

    up

--- metrics/edit_eval.py
+++ metrics/edit_eval.py
@@ -92,7 +92,7 @@ def main():
             raise ValueError
 
         # print(instruct_text[0].lower())
-        if instruct_text[0].lower().replace('\n', '') != '':
+        if instruct_text[0].lower().replace('\n', '').replace('/n','') != '':
             id_sim_dict[this_key_word].append(id_sim.item())
             all_id_sims.append(id_sim.item())
         # print(img_path, id_sim.item())
