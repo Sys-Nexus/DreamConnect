636827048d095c4793073c4c130dd3959d060f1e
sunyasheng <1044705955@qq.com>
2024-04-18 18:22:07 +0900

    up

--- metrics/batch_edit_eval.sh
+++ metrics/batch_edit_eval.sh
@@ -1,11 +1,11 @@
 log_dirs=(
-    # logs/test_conv_adaptor_idback_test_conv_adaptor_idback_2024-04-17/visualize/images/val
+    logs/test_conv_adaptor_idback_test_conv_adaptor_idback_2024-04-17/visualize/images/val
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-13/visualize/images/val 
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_pix2pix
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_inst_dif
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_magic_brush
-    logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
+    # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
 )
 
 
@@ -16,18 +16,18 @@ eval_methods=(
     'dino'
     'clip'
 )
-for eval_mode in ${eval_modes[@]}
-do
-    echo 'eval_mode: '$eval_mode
-    for eval_method in ${eval_methods[@]}
-    do
-        echo 'eval_method: '$eval_method
-        for root_dir in ${log_dirs[@]}
-        do
-            python  metrics/edit_eval.py --eval_method ${eval_method} --eval_mode ${eval_mode} --root_dir ${root_dir}
-        done
-    done
-done
+# for eval_mode in ${eval_modes[@]}
+# do
+#     echo 'eval_mode: '$eval_mode
+#     for eval_method in ${eval_methods[@]}
+#     do
+#         echo 'eval_method: '$eval_method
+#         for root_dir in ${log_dirs[@]}
+#         do
+#             python  metrics/edit_eval.py --eval_method ${eval_method} --eval_mode ${eval_mode} --root_dir ${root_dir}
+#         done
+#     done
+# done
 
 
 eval_modes=(
