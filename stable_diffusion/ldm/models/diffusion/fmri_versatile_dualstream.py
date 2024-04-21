21010e37c8ed196e906e1b5e07830f3326e109a0
sunyasheng <1044705955@qq.com>
2024-04-21 09:03:43 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -762,6 +762,41 @@ class DualLDM(LatentDiffusion):
             file.write(instruct_cap)
 
     @torch.no_grad()
+    def save_fmri_vae(self, batch, epoch_n, iter_n, batch_idx, model_wrap, model_wrap_cfg,
+                   save_dir, split,
+                   cfg_text=7.5, cfg_fmri=1.5,
+                   cfg_text_edit=None, cfg_image_edit=None,
+                   delay_t=None,
+                   N=1, n_row=4, sample=True, 
+                   steps=100, ddim_eta=1., return_keys=None,
+                   quantize_denoised=True, inpaint=False,
+                   prospect_words=None, 
+                   is_inst_edit=False,
+                   is_inst_gen=False,
+                   layout_in=None):
+        if self.model is None: return
+        self.model.eval()
+        s = batch['s'][0]
+
+        N = min(batch['image'].shape[0], N)
+        z_gt, c, x, xrec, xc = self.get_input(batch, self.first_stage_key, force_c_encode=True,
+                                               bs=N, uncond=0, return_original_cond=True, 
+                                               return_first_stage_outputs=True,
+                                               prospect_words=prospect_words)
+        
+        if layout_in is not None:
+            sz = 512
+            layout_in = F.interpolate(layout_in, (sz,sz))
+            layout_concat = self.encode_first_stage(layout_in).mode().detach()
+
+        init_latent = torch.cat(c["c_crossattn_1"]["fmri_vae"],dim=0)
+        output_basename = '{:05d}.npy'.format(s)
+        output_path = os.path.join(root, output_basename)
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        import pdb; pdb.set_trace()
+        np.save(init_latent.cpu().numpy(), output_path)
+
+    @torch.no_grad()
     def log_images(self, batch, epoch_n, iter_n, batch_idx, model_wrap, model_wrap_cfg,
                    save_dir, split,
                    cfg_text=7.5, cfg_fmri=1.5,
@@ -791,8 +826,11 @@ class DualLDM(LatentDiffusion):
             layout_concat = self.encode_first_stage(layout_in).mode().detach()
 
         # import pdb; pdb.set_trace();
-        init_latent = torch.cat(c["c_crossattn_1"]["fmri_vae"],dim=0)
-        
+        if self.use_fmri_vae and split == 'val':
+            init_latent = batch['fmri_vae'][:N].to(z_gt)
+        else:
+            init_latent = torch.cat(c["c_crossattn_1"]["fmri_vae"],dim=0)
+
         self.device = z_gt.device
         self.sampler.model.model.diffusion_model.device = z_gt.device
         self.sampler.make_schedule(ddim_num_steps=self.ddim_steps, ddim_eta=self.ddim_eta, verbose=False)
@@ -802,7 +840,7 @@ class DualLDM(LatentDiffusion):
         
         c_w_uncond = copy.deepcopy(c)
 
-        import pdb; pdb.set_trace();
+        # import pdb; pdb.set_trace();
 
         if self.use_fmri_clip and split == 'val':
             c0 = batch['img_clip'][:N].to(c["c_crossattn_1"]["image_emb"][0])
@@ -944,7 +982,7 @@ class DualLDM(LatentDiffusion):
         os.makedirs(os.path.dirname(x_gen_path), exist_ok=True)
         torchvision.utils.save_image(x_gen*0.5+0.5, x_gen_path)
         self.model.train()
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
 
     def apply_model(self, x_noisy_gen, x_noisy_edit, t, cond=None, t_edit_in=None, is_save_intermediate=True, is_save_x0=False, 
                         is_return_x0=False, sqrt_one_minus_at=None, a_t=None, sqrt_one_minus_at_offset=None, a_t_offset=None, 
