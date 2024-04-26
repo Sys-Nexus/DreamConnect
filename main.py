ee62a4a6e2a2eacf5c1151d39df49757dde1c0d1
sunyasheng <1044705955@qq.com>
2024-04-26 09:03:16 +0900

    up

--- main.py
+++ main.py
@@ -277,6 +277,12 @@ def get_parser(**parser_kwargs):
         default='',
         help="",
     )
+    parser.add_argument(
+        "--whitelist_path",
+        type=str,
+        default='',
+        help="",
+    )
     return parser
 
 
@@ -404,15 +410,23 @@ class DataModuleFromConfig():
 
 def test_one_epoch(config, model, model_ema, data_loader, val_data_loader, optimizer, epoch, 
         lr_scheduler, scaler, model_wrap, model_wrap_cfg, save_dir, cfg_text, cfg_text_edit,
-        prospect_words=None, is_inst_edit=False, is_inst_gen=False, layout_in=None):
+        prospect_words=None, is_inst_edit=False, is_inst_gen=False, layout_in=None, whitelist_path=''):
     # import pdb; pdb.set_trace()
     model.eval()
     epoch, idx = 999999, 999999
+    whitelist = None
+    if os.path.exist(whitelist_path): 
+        with open(whitelist_path, 'r') as f:
+            whitelist = f.read().splitlines()
+            import pdb; pdb.set_trace()
+        
     with torch.no_grad():
         for val_idx, batch in enumerate(val_data_loader):
             batch_size = batch['image'].shape[0]
             if model_wrap is not None:
                 if is_inst_edit is True or is_inst_gen is True:
+                    if whitelist is not None and '{:06d}'.format(batch['s'][0]) not in whitelist: continue
+
                     model.log_images(batch, epoch, idx, val_idx, model_wrap, model_wrap_cfg, 
                                  save_dir, 'val', cfg_text=cfg_text, cfg_text_edit=cfg_text_edit, cfg_fmri=2.5,
                                  prospect_words=prospect_words, is_inst_edit=is_inst_edit, is_inst_gen=is_inst_gen)
@@ -847,6 +861,7 @@ if __name__ == "__main__":
                         prospect_words=prospect_words, 
                         is_inst_edit=opt.is_inst_edit,
                         is_inst_gen=opt.is_inst_gen,
+                        whitelist_path=opt.whitelist_path,
                         layout_in=layout_in)
 
     total_time = time.time() - start_time
