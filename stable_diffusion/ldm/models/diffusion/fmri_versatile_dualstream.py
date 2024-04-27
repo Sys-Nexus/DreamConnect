81add82e1b84bc54782c674c33b62ea90d795c18
sunyasheng <1044705955@qq.com>
2024-04-27 21:58:10 +0900

    up

--- stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
+++ stable_diffusion/ldm/models/diffusion/fmri_versatile_dualstream.py
@@ -929,7 +929,11 @@ class DualLDM(LatentDiffusion):
         unconditional_guidance_scale_edit = None
 
         # import pdb; pdb.set_trace();
-        context_mask = torch.zeros((N,1,256,256)).to(c0)
+        context_mask = torch.zeros((N,1,16,16)).to(c0) 
+        context_mask = F.interpolate(context_mask, (16, 16)).flatten(0).unsqueeze(0)
+        context_mask = context_mask.flatten()
+        
+        # context_mask = torch.zeros((N,1,256,256)).to(c0)
         c_w_uncond['context_mask'] = torch.cat([context_mask, context_mask, context_mask], 0)
         if cfg_text_edit is not None and cfg_image_edit is not None:
             c_w_uncond["c_crossattn_1"]["image_emb"] = [torch.cat([uncond_c0, c0, c0], 0)]
