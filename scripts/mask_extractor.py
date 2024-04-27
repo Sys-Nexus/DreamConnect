9533b22bc42b5b546472f881620f3d9c63987023
sunyasheng <1044705955@qq.com>
2024-04-27 13:54:15 +0900

    up

--- scripts/mask_extractor.py
+++ scripts/mask_extractor.py
@@ -1,5 +1,6 @@
 from segment_anything import SamPredictor, sam_model_registry
 from PIL import Image
+import cv2
 
 
 def main():
@@ -8,7 +9,8 @@ def main():
     sam = sam_model_registry[model_type](checkpoint=ckpt_path)
     predictor = SamPredictor(sam)
     your_image_path = '../../Diffusion/InstructDiffusion/teaser_samples/all_iter-999999_ep-999999_bidx-000234-020064.png'
-    your_image = Image.open(your_image_path)
+    # your_image = Image.open(your_image_path)
+    your_image = cv2.imread(your_image_path)
     predictor.set_image(your_image)
     masks, _, _ = predictor.predict('cat.')
     import pdb; pdb.set_trace()
