888ea3ac35472b2e31ba180195595643cf398c08
sunyasheng <1044705955@qq.com>
2024-04-28 09:34:15 +0900

    up

--- scripts/mask_extractor.py
+++ scripts/mask_extractor.py
@@ -10,10 +10,10 @@ import numpy as np
 #     env_cmd = 'conda activate tformer4192'
 #     os.system(env_cmd)
 
-#     text_prompt = ''
-#     image_path = ''
-#     output_dir = ''
-#     exe_cmd = 'python grounded_sam_demo.py --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py --grounded_checkpoint groundingdino_swint_ogc.pth   --sam_checkpoint sam_vit_h_4b8939.pth   --input_image {} --output_dir {} --box_threshold 0.3 --text_threshold 0.25 --text_prompt {} --device cuda'.format(image_path, text_prompt, output_dir)
+    text_prompt = 'cat'
+    image_path = '/data/yashengsun/Proj/Diffusion/InstructDiffusion/teaser_samples/all_iter-999999_ep-999999_bidx-000234-020064.png'
+    output_dir = 'output'
+    exe_cmd = 'python grounded_sam_demo.py --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py --grounded_checkpoint groundingdino_swint_ogc.pth   --sam_checkpoint sam_vit_h_4b8939.pth   --input_image {} --output_dir {} --box_threshold 0.3 --text_threshold 0.25 --text_prompt {} --device cuda'.format(image_path, text_prompt, output_dir)
 
 
 if __name__ == '__main__':
