4c17c3d8790c7aea8d8fccef07b9c73bbf7b9fd8
sunyasheng <1044705955@qq.com>
2024-04-28 09:39:10 +0900

    up

--- scripts/mask_extractor.py
+++ scripts/mask_extractor.py
@@ -13,7 +13,7 @@ def main():
     text_prompt = 'cat'
     image_path = '/data/yashengsun/Proj/Diffusion/InstructDiffusion/teaser_samples/all_iter-999999_ep-999999_bidx-000234-020064.png'
     output_dir = 'output'
-    exe_cmd = 'python grounded_sam_demo.py --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py --grounded_checkpoint groundingdino_swint_ogc.pth   --sam_checkpoint sam_vit_h_4b8939.pth   --input_image {} --output_dir {} --box_threshold 0.3 --text_threshold 0.25 --text_prompt {} --device cuda'.format(image_path, text_prompt, output_dir)
+    exe_cmd = 'python grounded_sam_demo.py --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py --grounded_checkpoint groundingdino_swint_ogc.pth   --sam_checkpoint sam_vit_h_4b8939.pth   --input_image {} --output_dir {} --box_threshold 0.3 --text_threshold 0.25 --text_prompt {} --device cuda'.format(image_path, output_dir, text_prompt)
     os.system(exe_cmd)
 
 
