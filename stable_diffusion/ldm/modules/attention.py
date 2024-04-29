c45f0fb4393c0aca254e04054c18a735444731b3
sunyasheng <1044705955@qq.com>
2024-04-29 11:18:51 +0900

    up

--- stable_diffusion/ldm/modules/attention.py
+++ stable_diffusion/ldm/modules/attention.py
@@ -224,24 +224,25 @@ class CrossAttention(nn.Module):
             mask[mask < thres] = 0
             # import pdb; pdb.set_trace()
             mask = mask.unsqueeze(0).unsqueeze(-1)
-            sim_fg = sim_fg + mask.masked_fill(mask == 0, torch.finfo(sim.dtype).min)
-            sim_bg = sim_bg + mask.masked_fill(mask == 1, torch.finfo(sim.dtype).min)
+            sim[:sim.shape[0]//3] = sim_fg * mask + sim_bg * (1 - mask)
+            # sim_fg = sim_fg + mask.masked_fill(mask == 0, torch.finfo(sim.dtype).min)
+            # sim_bg = sim_bg + mask.masked_fill(mask == 1, torch.finfo(sim.dtype).min)
 
-            attn_fg = torch.softmax(sim_fg.float(), dim=-1).type(sim_fg.dtype)
-            attn_bg = torch.softmax(sim_bg.float(), dim=-1).type(sim_bg.dtype)
+            # attn_fg = torch.softmax(sim_fg.float(), dim=-1).type(sim_fg.dtype)
+            # attn_bg = torch.softmax(sim_bg.float(), dim=-1).type(sim_bg.dtype)
 
-            out_fg = einsum('b i j, b j d -> b i d', attn_fg, v[:v.shape[0]//3])
-            out_bg = einsum('b i j, b j d -> b i d', attn_bg, v[:v.shape[0]//3])
+            # out_fg = einsum('b i j, b j d -> b i d', attn_fg, v[:v.shape[0]//3])
+            # out_bg = einsum('b i j, b j d -> b i d', attn_bg, v[:v.shape[0]//3])
             
-            out_main = out_fg * mask + out_bg * (1 - mask)
-            out_main = rearrange(out_main, '(b h) n d -> b n (h d)', h=h)
+            # out_main = out_fg * mask + out_bg * (1 - mask)
+            # out_main = rearrange(out_main, '(b h) n d -> b n (h d)', h=h)
             
         attn = torch.softmax(sim.float(), dim=-1).type(sim.dtype)
         out = einsum('b i j, b j d -> b i d', attn, v)
         out = rearrange(out, '(b h) n d -> b n (h d)', h=h)
 
-        if context_mask is not None:
-           out[:1] = out_main[:1]
+        # if context_mask is not None:
+        #    out[:1] = out_main[:1]
 
         return self.to_out(out)
 
