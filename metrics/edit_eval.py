640d97dae6ca88210865a3f50289eb1d251672e4
sunyasheng <1044705955@qq.com>
2024-05-02 10:13:58 +0900

    up

--- metrics/edit_eval.py
+++ metrics/edit_eval.py
@@ -43,6 +43,7 @@ def main():
     parser.add_argument('--root_dir', type=str, default='logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit')
     parser.add_argument('--text_dir', type=str, default='logs/test_conv_adaptor_test_conv_adaptor_2024-04-13/visualize/images/val')
     parser.add_argument('--eval_mode', type=str, default='id_sim')
+    parser.add_argument('--is_mask_enhance', type=int, default=1)
     args = parser.parse_args()
 
     if args.eval_mode == 'id_sim':
@@ -62,6 +63,10 @@ def main():
         pass
     else:
         raise ValueError
+
+    if args.is_mask_enhance:
+        with open('res_dict_{}.pkl'.format(args.eval_mode), 'rb') as f:
+            baseline_eval_res = pickle.load(f)
 
     img_paths = sorted(glob.glob(os.path.join(args.root_dir, 'all_iter*.png')))
     key_words = ['make', 'replace', 'turn', 'change', 'remove', 'add', 'insert', 'swap', 'switch', 'put', 'cut']
@@ -115,6 +120,10 @@ def main():
             instr_feat = sim_evaluator.encode_text(output_text) - sim_evaluator.encode_text(input_text)
             delta_feat = edit_feat - input_feat
             id_sim = F.cosine_similarity(delta_feat / delta_feat.norm(dim=1, keepdim=True), instr_feat)
+            if args.is_mask_enhance:
+                baseline_id_sim = baseline_eval_res['inst_sim']
+                import pdb; pdb.set_trace()
+
         elif args.eval_mode == 'fid':
             id_sim = torch.zeros(1)
         else:
@@ -160,8 +169,8 @@ def main():
     else:
         pass
     
-    with open('res_dict_{}.pkl'.format(args.eval_mode), 'wb') as f:
-        pickle.dump(res_dict, f)
+    # with open('res_dict_{}.pkl'.format(args.eval_mode), 'wb') as f:
+    #     pickle.dump(res_dict, f)
 
     # id_sim_dict = {}
     # for key_word in key_words:
