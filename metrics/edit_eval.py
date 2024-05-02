f0980db21bff2c301596c6fe94dcfa8c60c3eaf3
sunyasheng <1044705955@qq.com>
2024-05-02 10:32:22 +0900

    up

--- metrics/edit_eval.py
+++ metrics/edit_eval.py
@@ -121,7 +121,7 @@ def main():
             delta_feat = edit_feat - input_feat
             id_sim = F.cosine_similarity(delta_feat / delta_feat.norm(dim=1, keepdim=True), instr_feat)
             if args.is_mask_enhance:
-                baseline_id_sim = baseline_eval_res[self.eval_mode][img_path]
+                baseline_id_sim = baseline_eval_res[self.eval_mode][os.path.basename(img_path)]
                 import pdb; pdb.set_trace()
 
         elif args.eval_mode == 'fid':
@@ -169,8 +169,8 @@ def main():
     else:
         pass
     
-    with open('res_dict_{}.pkl'.format(args.eval_mode), 'wb') as f:
-        pickle.dump(res_dict, f)
+    # with open('res_dict_{}.pkl'.format(args.eval_mode), 'wb') as f:
+    #     pickle.dump(res_dict, f)
 
     # id_sim_dict = {}
     # for key_word in key_words:
