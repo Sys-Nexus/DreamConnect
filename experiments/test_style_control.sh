ce6a680846c95200ddcf036a5d99555ddd81bdeb
sunyasheng <1044705955@qq.com>
2024-05-07 21:08:00 +0900

    up

--- experiments/test_style_control.sh
+++ experiments/test_style_control.sh
@@ -1,7 +1,5 @@
 
-# jobname=${1:-'prospect_test_conv_adaptor_mixing0_image'}
-# jobname=${1:-'prospect_test_conv_adaptor_mixing1_text'}
-jobname=${1:-'prospect_test_conv_adaptor'}
+jobname=${1:-'prospect_test_res_value_inject_idback_css15'}
 filter_mode=${2:-'tune_sideconv'} #do not use no_filter, which will fail the memory
 
 gpus=${3:-'0,'}
@@ -28,62 +26,62 @@ export LD_LIBRARY_PATH=/usr/local/cuda-11.3/lib64:$LD_LIBRARY_PATH
 current_date=$(date +"%Y-%m-%d")
 
 prospect_ckpt_paths=(
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/starryNew2024-03-04T23-34-17_starryNew/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/window_grille2024-03-04T23-28-37_window_grille/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/circuit2024-03-04T23-23-03_circuit/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/blackstrokes2024-03-04T23-17-24_blackstrokes/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/udnie2024-03-04T23-11-50_udnie/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/cloud2024-03-04T23-06-12_cloud/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/stars2024-03-04T23-00-36_stars/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/leaves2024-03-04T22-54-59_leaves/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/yellow_square2024-03-04T22-49-23_yellow_square/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/occean2024-03-04T22-43-46_occean/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/starryNew2024-03-04T23-34-17_starryNew/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/window_grille2024-03-04T23-28-37_window_grille/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/circuit2024-03-04T23-23-03_circuit/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/blackstrokes2024-03-04T23-17-24_blackstrokes/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/udnie2024-03-04T23-11-50_udnie/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/cloud2024-03-04T23-06-12_cloud/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/stars2024-03-04T23-00-36_stars/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/leaves2024-03-04T22-54-59_leaves/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/yellow_square2024-03-04T22-49-23_yellow_square/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/occean2024-03-04T22-43-46_occean/checkpoints/embeddings.pt
   /data/yashengsun/Proj/MMEdit/ProSpect/logs/rain_princess2024-03-04T22-38-07_rain_princess/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/wave2024-03-04T22-32-31_wave/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/202004282208292024-03-04T22-26-58_20200428220829/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/colorful_cubes2024-03-04T22-21-24_colorful_cubes/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/bricks2024-03-04T22-15-50_bricks/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs/feathers2024-03-04T22-10-18_feathers/checkpoints/embeddings.pt
-  # /data/yashengsun/Proj/MMEdit/ProSpect/logs_backup0305/kanagawa2024-02-29T06-01-27_kanagawa/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/wave2024-03-04T22-32-31_wave/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/202004282208292024-03-04T22-26-58_20200428220829/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/colorful_cubes2024-03-04T22-21-24_colorful_cubes/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/bricks2024-03-04T22-15-50_bricks/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs/feathers2024-03-04T22-10-18_feathers/checkpoints/embeddings.pt
+  /data/yashengsun/Proj/MMEdit/ProSpect/logs_backup0305/kanagawa2024-02-29T06-01-27_kanagawa/checkpoints/embeddings.pt
 )
 
 style_names=(
-  # 'starryNew'
-  # 'window_grille'
-  # 'circuit'
-  # 'blackstrokes'
-  # 'udnie'
-  # 'cloud'
-  # 'stars'
-  # 'leaves'
-  # 'yellow_square'
-  # 'occean'
+  'starryNew'
+  'window_grille'
+  'circuit'
+  'blackstrokes'
+  'udnie'
+  'cloud'
+  'stars'
+  'leaves'
+  'yellow_square'
+  'occean'
   'rain_princess'
-  # 'wave'
-  # '20200428220829'
-  # 'colorful_cubes'
-  # 'bricks'
-  # 'feathers'
-  # 'kanagawa'
+  'wave'
+  '20200428220829'
+  'colorful_cubes'
+  'bricks'
+  'feathers'
+  'kanagawa'
 )
 
 cfg_text_edits=(
-  # 14.0
-  # 7.0
-  # 14.0
-  # 21.0
-  # 7.0
-  # 7.0
-  # 7.0
-  # 14.0
-  # 7.0
+  14.0
   7.0
-  # 7.0
-  # 7.0
-  # 4.0
-  # 7.0
-  # 7.0
-  # 7.0
+  14.0
+  21.0
+  7.0
+  7.0
+  7.0
+  14.0
+  7.0
+  7.0
+  7.0
+  7.0
+  4.0
+  7.0
+  7.0
+  7.0
 )
 
 
@@ -94,7 +92,7 @@ cfg_text_edits=(
 # prospect_ckpt_path='/data/yashengsun/Proj/MMEdit/ProSpect/logs_backup0305/kanagawa2024-02-29T06-01-27_kanagawa/checkpoints/embeddings.pt'
 # ${HOME}/.local/bin/wandb login 56d149bd571b8312fbca5e3802d7859909ea00c1
 
-whitelist_path='configs/test/style_control_list.txt'
+# whitelist_path='configs/test/style_control_list.txt'
 
 length=${#style_names[@]}
 
@@ -119,8 +117,8 @@ for ((i = 0; i < length; i++)); do
       --prospect_ckpt_path ${prospect_ckpt_path} \
       --cfg_text_edit ${cfg_text_edit} \
       --is_inst_gen True \
-      --is_inst_edit True \
-      --whitelist_path ${whitelist_path}
+      --is_inst_edit True #\
+      # --whitelist_path ${whitelist_path}
 done
 
 
