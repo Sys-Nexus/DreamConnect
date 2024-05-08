b9342065b06eaf5fe02543b68ed6f35bf263caed
sunyasheng <1044705955@qq.com>
2024-05-08 11:13:23 +0900

    up

--- metrics/batch_edit_eval.sh
+++ metrics/batch_edit_eval.sh
@@ -14,7 +14,13 @@ log_dirs=(
     # logs/test_conv_adaptor_test_conv_adaptor_2024-04-08/visualize/images/val_sdedit
 
     # mask enhance version
-    logs/test_res_value_inject_idback_css15_test_res_value_inject_idback_css15_2024-05-01/visualize/images/val
+    # logs/test_res_value_inject_idback_css15_test_res_value_inject_idback_css15_2024-05-01/visualize/images/val
+
+    # ablation
+    logs/test_res_value_inject_idback_css25_test_res_value_inject_idback_css25_2024-05-05/visualize/images/val
+    logs/test_res_value_inject_idback_css5_test_res_value_inject_idback_css5_2024-05-05/visualize/images/val
+    logs/test_res_value_inject_idback_css15_test_res_value_inject_idback_css15_2024-05-04/visualize/images/val
+    logs/test_res_value_inject_idback_css1_test_res_value_inject_idback_css1_2024-05-04/visualize/images/val
 )
 
 
@@ -38,25 +44,25 @@ log_dirs=(
 # done
 
 
-# eval_modes=(
-#     'id_sim'
-# )
-# eval_methods=(
-#     'clip'
-#     'dino'
-# )
-# for eval_mode in ${eval_modes[@]}
-# do
-#     echo 'eval_mode: '$eval_mode
-#     for eval_method in ${eval_methods[@]}
-#     do
-#         echo 'eval_method: '$eval_method
-#         for root_dir in ${log_dirs[@]}
-#         do
-#             python  metrics/edit_eval.py --eval_method ${eval_method} --eval_mode ${eval_mode} --root_dir ${root_dir}
-#         done
-#     done
-# done
+eval_modes=(
+    'id_sim'
+)
+eval_methods=(
+    'clip'
+    'dino'
+)
+for eval_mode in ${eval_modes[@]}
+do
+    echo 'eval_mode: '$eval_mode
+    for eval_method in ${eval_methods[@]}
+    do
+        echo 'eval_method: '$eval_method
+        for root_dir in ${log_dirs[@]}
+        do
+            python  metrics/edit_eval.py --eval_method ${eval_method} --eval_mode ${eval_mode} --root_dir ${root_dir}
+        done
+    done
+done
 
 
 eval_modes=(
