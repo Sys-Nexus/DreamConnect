4b36b02b75726cdc4e0bd47d1e9f4e533f195194
sunyasheng <1044705955@qq.com>
2024-08-17 09:12:51 +0900

    up

--- experiments/test_language_control.sh
+++ experiments/test_language_control.sh
@@ -1,17 +1,20 @@
 
 # jobname=${1:-'test_conv_adaptor'}
 # jobname=${1:-'test_zero_adaptor_idback'} # test_res_value_inject_idback_css15
-jobname=${1:-'test_res_value_inject_idback_css15'}
 # jobname=${1:-'test_conv_adaptor_mixing1_text'}
+
+jobname=${1:-'test_res_value_inject_idback_css15'}
 filter_mode=${2:-'tune_sideconv'} #do not use no_filter, which will fail the memory
+
+
+# cfg_text_edit=${6:-20.0}
+# cfg_text_edit=${6:-1.5}
 
 gpus=${3:-'0,'}
 master_port=${4:-'27699'}
 isTrain=${5:-0}
 cfg_text=${6:-7.5}
-# cfg_text_edit=${6:-20.0}
 cfg_text_edit=${6:-7.5}
-# cfg_text_edit=${6:-1.5}
 mode=${7:-'single'}
 vis=${8:-1}
 
@@ -35,15 +38,15 @@ exp_name=${jobname}"_"${current_date}
 # prospect_ckpt_path='/data/yashengsun/Proj/MMEdit/ProSpect/logs/apple2024-02-28T22-30-10_apple/checkpoints/embeddings.pt'
 # prospect_ckpt_path='/data/yashengsun/Proj/MMEdit/ProSpect/logs/panda2024-02-29T05-29-09_panda/checkpoints/embeddings.pt'
 # prospect_ckpt_path='/data/yashengsun/Proj/MMEdit/ProSpect/logs_backup0305/panda2024-02-29T05-29-09_panda/checkpoints/embeddings.pt'
-prospect_ckpt_path='/data/yashengsun/Proj/MMEdit/ProSpect/logs_backup0305/kanagawa2024-02-29T06-01-27_kanagawa/checkpoints/embeddings.pt'
-layout_path='/data/yashengsun/Proj/MMEdit/ProSpect/images4prospect/layout_collections/baozi/baozi.jpg'
+# prospect_ckpt_path='/data/yashengsun/Proj/MMEdit/ProSpect/logs_backup0305/kanagawa2024-02-29T06-01-27_kanagawa/checkpoints/embeddings.pt'
+# layout_path='/data/yashengsun/Proj/MMEdit/ProSpect/images4prospect/layout_collections/baozi/baozi.jpg'
 # resume_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/checkpoints/bohan_recon/'
-resume_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/logs/wo_prospect_ctx_adaptor_bs4_wo_prospect_ctx_adaptor_bs4_2024-03-13/checkpoints/ckpt_epoch_5'
+# resume_path='/data/yashengsun/Proj/MMEdit/fMRIInstructDiffusion/logs/wo_prospect_ctx_adaptor_bs4_wo_prospect_ctx_adaptor_bs4_2024-03-13/checkpoints/ckpt_epoch_5'
 
 # ${HOME}/.local/bin/wandb login 56d149bd571b8312fbca5e3802d7859909ea00c1
 
 # whitelist_path for mask enhance
-whitelist_path='configs/test/mask_enhance_list.txt'  # essentially a whitelist for us to screen the key 
+# whitelist_path='configs/test/mask_enhance_list.txt'  # essentially a whitelist for us to screen the key 
 
 CUDA_VISIBLE_DEVICES=${gpus} ${run_cmd} main.py --name ${exp_name} \
     --base configs/test/${jobname}.yaml \
@@ -56,8 +59,8 @@ CUDA_VISIBLE_DEVICES=${gpus} ${run_cmd} main.py --name ${exp_name} \
     --vis ${vis} \
     --isTrain ${isTrain} \
     --cfg_text ${cfg_text} \
-    --prospect_ckpt_path ${prospect_ckpt_path} \
     --cfg_text_edit ${cfg_text_edit} #\
+    # --prospect_ckpt_path ${prospect_ckpt_path} \
     # --whitelist_path ${whitelist_path}
     # --layout_path ${layout_path} #\
     # --is_inst_gen True
